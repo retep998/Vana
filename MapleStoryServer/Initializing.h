edb060b74186e048e17ae4239683069f2b68e6c2
pawitp <pawitp@23f3cdc0-e6d1-42be-a0c1-51f4125517bb>
2008-04-27 07:19:33 +0000

    Changed initializing to a namespace, other static class will follow
    git-svn-id: https://dev.chiasoft.net:8443/svn/svn_vana/trunk@14 23f3cdc0-e6d1-42be-a0c1-51f4125517bb

--- MapleStoryServer/Initializing.h
+++ MapleStoryServer/Initializing.h
@@ -1,19 +1,18 @@
 #ifndef INITIALIZE_H
 #define INITIALIZE_H
 
-class Initializing {
-public:
-	static void initializing();
-	static void initializeMySQL();
-	static void initializeMobs();
-	static void initializeDrops();
-	static void initializeMaps();
-	static void initializeEquips();
-	static void initializeShops();
-	static void initializeItems();
-	static void initializeQuests();
-	static void initializeSkills();
-	static void initializeTimers();
+namespace Initializing {
+	void initializing();
+	void initializeMySQL();
+	void initializeMobs();
+	void initializeDrops();
+	void initializeMaps();
+	void initializeEquips();
+	void initializeShops();
+	void initializeItems();
+	void initializeQuests();
+	void initializeSkills();
+	void initializeTimers();
 };
 
 #endif
