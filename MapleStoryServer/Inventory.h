54fb7a803e17dd5077485290a8ba0a65a784703e
pawitp <pawitp@23f3cdc0-e6d1-42be-a0c1-51f4125517bb>
2008-04-27 09:20:44 +0000

    Converted Inventory into a namespace
    git-svn-id: https://dev.chiasoft.net:8443/svn/svn_vana/trunk@26 23f3cdc0-e6d1-42be-a0c1-51f4125517bb

--- MapleStoryServer/Inventory.h
+++ MapleStoryServer/Inventory.h
@@ -7,24 +7,23 @@ class Player;
 struct Equip;
 struct Item;
 
-class Inventory {
-public:
-	static void useShop(Player* player, unsigned char* packet);
-	static void itemMove(Player* player, unsigned char* packet);
-	static int findSlot(Player* player, int itemid ,char inv, short amount);
-	static Equip* setEquipStats(Player* player, int equipid);
-	static void addEquip(Player* player, Equip* equip, bool is=0);
-	static void addItem(Player* player, Item* item, bool is=0);
-	static void addNewItem(Player* player, int item, int howmany);
-	static void takeItem(Player* player, int item, int howmany);
-	static void useItem(Player* player, unsigned char* packet);
-	static void takeItemSlot(Player* player, short slot, char inv, short amount);
-	static void useChair(Player* player, unsigned char* packet);
-	static void stopChair(Player* player, unsigned char* packet);
-	static int isCash(int itemid);
-	static void useSummonBag(Player* player, unsigned char* packet);
-	static void useReturnScroll(Player* player, unsigned char* packet);
-	static void useScroll(Player* player, unsigned char* packet);
+namespace Inventory {
+	extern int findSlot(Player* player, int itemid ,char inv, short amount);
+	extern int isCash(int itemid);
+	extern Equip* setEquipStats(Player* player, int equipid);
+	void useShop(Player* player, unsigned char* packet);
+	void itemMove(Player* player, unsigned char* packet);
+	void addEquip(Player* player, Equip* equip, bool is=0);
+	void addItem(Player* player, Item* item, bool is=0);
+	void addNewItem(Player* player, int item, int howmany);
+	void takeItem(Player* player, int item, int howmany);
+	void useItem(Player* player, unsigned char* packet);
+	void takeItemSlot(Player* player, short slot, char inv, short amount);
+	void useChair(Player* player, unsigned char* packet);
+	void stopChair(Player* player, unsigned char* packet);
+	void useSummonBag(Player* player, unsigned char* packet);
+	void useReturnScroll(Player* player, unsigned char* packet);
+	void useScroll(Player* player, unsigned char* packet);
 };
 
 #endif
