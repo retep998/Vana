81b88e6cd18a62f0af6dc71965ad2c036eaecc2b
pawitp <pawitp@23f3cdc0-e6d1-42be-a0c1-51f4125517bb>
2008-05-06 14:13:28 +0000

    Cleaned up PacketHandler.h
    git-svn-id: https://dev.chiasoft.net:8443/svn/svn_vana/trunk@110 23f3cdc0-e6d1-42be-a0c1-51f4125517bb

--- src/Connection/PacketHandler.h
+++ src/Connection/PacketHandler.h
@@ -22,8 +22,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include "../Decoder/MapleEncryption.h"
 #include "Selector.h"
 #include "AbstractPlayer.h"
-//#include "Decoder.h"
-//#include "GameManager.h"
 
 class PacketHandler: public Selector::SelectHandler {
 public:
@@ -39,25 +37,6 @@ private:
 	AbstractPlayer* player;
 	Decoder* decoder;
 	int socket;
-	//PlayerData pd;
-	/*void HandleRequest(unsigned char* packet, int size, PlayerData &pd){
-		short header = packet[0] + packet[1]*0x100;
-		switch(header){
-			case 0x01: GameManager::Login((const char*)packet+2, size-2, pd); break;
-			case 0x04: GameManager::ChannelSelect(packet+2, pd); break;
-			case 0x05: GameManager::SelectWorld(packet+2, pd); break;
-			case 0x07: GameManager::SetGender(packet+2 ,pd); break;
-			case 0x08: GameManager::HandleLogin(pd, packet+2); break;
-			case 0x09: GameManager::RegisterPIN(packet+2, pd); break;
-			case 0x0a: GameManager::ShowWorld(pd); break;
-			case 0x0b: GameManager::ShowWorld(pd); break;
-			case 0x0f: GameManager::ConnectGame(packet+2, pd); break;
-			case 0x11: GameManager::CheckCharacterName(packet+2,pd); break;
-			case 0x15: GameManager::CreateCharacter(packet+2,pd); break;
-			case 0x16: GameManager::DeleteCharacter(packet+2,pd); break;
-			case 0x1b: GameManager::LoginBack(pd); break;
-		}
-	}*/
 };
 
 #endif
