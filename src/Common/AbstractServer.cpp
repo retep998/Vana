3e18af66066aeb6b4ea2868ca3b122b281f945b9
pawitp <pawitp@09c1253f-3d09-8e40-9719-4320e56601b5>
2008-05-07 10:15:25 +0000

    Committing the forgotten AbstractServer.cpp/h
    git-svn-id: svn://70.113.44.214/VanaMirror/trunk@127 09c1253f-3d09-8e40-9719-4320e56601b5

---
+++ src/Common/AbstractServer.cpp
@@ -0,0 +1,32 @@
+/*
+Copyright (C) 2008 Vana Development Team
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; version 2
+of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+#include "AbstractServer.h"
+
+AbstractServer::AbstractServer() {
+	srand((unsigned char)time(0));
+}
+
+void AbstractServer::initialize() {
+	loadData();
+	loadConfig();
+
+	int iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
+	// if (iResult != NO_ERROR)  printf("Error at WSAStartup()\n"); //TODO: Throw exception
+
+	listen();
+}
---
+++ src/Common/AbstractServer.h
@@ -0,0 +1,39 @@
+/*
+Copyright (C) 2008 Vana Development Team
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; version 2
+of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+#ifndef ABSTRACTSERVER_H
+#define ABSTRACTSERVER_H
+
+#include "Connection/Acceptor.h"
+#include <time.h>
+
+class AbstractServer {
+public:
+	void initialize();
+	virtual void listen() = 0;
+	virtual void loadConfig() = 0;
+	virtual void loadData() = 0;
+
+	Selector* selector;
+	WSADATA wsaData;
+protected:
+	AbstractServer();
+
+	int _port;
+};
+
+#endif
