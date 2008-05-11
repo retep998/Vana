07f3dc57edbc097dcd3769c4d357697b23668483
pawitp <pawitp@23f3cdc0-e6d1-42be-a0c1-51f4125517bb>
2008-05-11 02:56:21 +0000

    Added connector (untested)
    git-svn-id: https://dev.chiasoft.net:8443/svn/svn_vana/trunk@175 23f3cdc0-e6d1-42be-a0c1-51f4125517bb

---
+++ src/Common/Connection/Connector.cpp
@@ -0,0 +1,47 @@
+/*
+Copyright (C) 2008 Vana Development Team
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; version 2
+of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+#include "Connector.h"
+#include "PacketHandler.h"
+#include <stdio.h>
+#include <Winsock2.h>
+
+Connector::Connector(char *ip, int port, AbstractPlayerFactory* apf) {
+	int iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
+	if (iResult != NO_ERROR)  printf("Error at WSAStartup()\n"); //TODO: Throw exception
+
+	SOCKET sock = socket (AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (sock == INVALID_SOCKET) {
+		printf ("socket error: %d\n", WSAGetLastError());
+		return;
+	}
+
+	sockaddr_in service;
+	service.sin_family = AF_INET;
+	service.sin_addr.s_addr = inet_addr(ip);
+	service.sin_port = htons(port);
+
+	if (connect(sock, (SOCKADDR*) &service, sizeof(service)) == SOCKET_ERROR) {
+		printf("connect() error: %d\n", WSAGetLastError());
+		return;
+	}
+
+	AbstractPlayer* player = apf->createPlayer();
+	PacketHandler* ph = new PacketHandler(sock, player, true);
+	player->setPacketHandler(ph);
+	Selector::Instance()->registerSocket(sock, true, false, true, ph);
+}
