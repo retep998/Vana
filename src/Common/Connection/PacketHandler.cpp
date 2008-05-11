99175f3e10574bff981fdad2daec10455cc5fffd
pawitp <pawitp@23f3cdc0-e6d1-42be-a0c1-51f4125517bb>
2008-05-11 00:49:18 +0000

    Clean up
    git-svn-id: https://dev.chiasoft.net:8443/svn/svn_vana/trunk@169 23f3cdc0-e6d1-42be-a0c1-51f4125517bb

--- src/Common/Connection/PacketHandler.cpp
+++ src/Common/Connection/PacketHandler.cpp
@@ -16,7 +16,6 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 #include "PacketHandler.h"
-//#include "Decoder.h"
 #include <Winsock2.h>
 
 #define HEADER_LEN 4
