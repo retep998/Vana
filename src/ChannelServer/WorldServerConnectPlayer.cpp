49b07e8d5de704f6a1f2f66418caedb351ea913d
pawitp <pawitp@09c1253f-3d09-8e40-9719-4320e56601b5>
2008-05-12 12:57:52 +0000

    Files for previous commit
    git-svn-id: svn://70.113.44.214/VanaMirror/trunk@226 09c1253f-3d09-8e40-9719-4320e56601b5

---
+++ src/ChannelServer/WorldServerConnectPlayer.cpp
@@ -0,0 +1,32 @@
+/*
+Copyright (C) 2008 Vana Development Team
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; version 2
+of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+#include "WorldServerConnectPlayer.h"
+#include "WorldServerConnectHandler.h"
+#include "InterHeader.h"
+
+WorldServerConnectPlayer::WorldServerConnectPlayer() {
+	type = INTER_CHANNEL_SERVER;
+}
+
+void WorldServerConnectPlayer::realHandleRequest(unsigned char *buf, int len) {
+	short header = buf[0] + buf[1]*0x100;
+	switch(header) {
+		case INTER_LOGIN_CHANNEL_CONNECT: WorldServerConnectHandler::connectLogin(this, buf+2); break;
+		case INTER_WORLD_CONNECT: WorldServerConnectHandler::connect(this, buf+2); break;
+	}
+}
