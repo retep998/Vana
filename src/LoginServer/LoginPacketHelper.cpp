3faa17e8d08560466ef755408a1cc1e282d7271d
pawitp <pawitp@09c1253f-3d09-8e40-9719-4320e56601b5>
2008-05-13 13:11:39 +0000

    Refactored showCharacter(s)
    git-svn-id: svn://70.113.44.214/VanaMirror/trunk@249 09c1253f-3d09-8e40-9719-4320e56601b5

---
+++ src/LoginServer/LoginPacketHelper.cpp
@@ -0,0 +1,67 @@
+/*
+Copyright (C) 2008 Vana Development Team
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; version 2
+of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+#include "LoginPacketHelper.h"
+#include "Characters.h"
+#include "PacketCreator.h"
+
+void LoginPacketHelper::addCharacter(Packet &packet, Character charc) {
+	packet.addInt(charc.id);
+	packet.addString(charc.name, 12);
+	packet.addByte(0);
+	packet.addByte(charc.gender);
+	packet.addByte(charc.skin);
+	packet.addInt(charc.eyes);
+	packet.addInt(charc.hair);
+	packet.addInt(0);
+	packet.addInt(0);
+	packet.addInt(0);
+	packet.addInt(0);
+	packet.addInt(0);
+	packet.addInt(0);
+	packet.addByte(charc.level);
+	packet.addShort(charc.job);
+	packet.addShort(charc.str);
+	packet.addShort(charc.dex);
+	packet.addShort(charc.intt);
+	packet.addShort(charc.luk);
+	packet.addShort(charc.hp);
+	packet.addShort(charc.mhp);
+	packet.addShort(charc.mp);
+	packet.addShort(charc.mmp);
+	packet.addShort(charc.ap);
+	packet.addShort(charc.sp);
+	packet.addInt(charc.exp);
+	packet.addShort(charc.fame);
+	packet.addInt(charc.map);
+	packet.addByte(charc.pos);
+	packet.addByte(charc.gender);
+	packet.addByte(charc.skin);
+	packet.addInt(charc.eyes);
+	packet.addByte(1);
+	packet.addInt(charc.hair);
+	for(int j=0; j<(int)charc.equips.size(); j++){
+		packet.addByte(charc.equips[j].type);
+		packet.addInt(charc.equips[j].id);
+	}
+	packet.addShort(-1);
+	packet.addInt(0);
+	packet.addInt(0);
+	packet.addInt(0);
+	packet.addInt(0);
+	packet.addByte(0);
+}
