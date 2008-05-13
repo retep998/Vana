1e48934ebafdea01d71f7ee2b83ebd1871fcc6ff
pawitp <pawitp@09c1253f-3d09-8e40-9719-4320e56601b5>
2008-05-13 23:56:52 +0000

    Changed login equip packet structure to conform to GMS struct
    git-svn-id: svn://70.113.44.214/VanaMirror/trunk@252 09c1253f-3d09-8e40-9719-4320e56601b5

--- src/LoginServer/Characters.cpp
+++ src/LoginServer/Characters.cpp
@@ -19,7 +19,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 void Characters::showEquips(int id, vector <CharEquip> &vec){
 	mysqlpp::Query query = db.query();
-	query << "SELECT equipid, type, pos FROM equip WHERE charid = " << mysqlpp::quote << id << " AND pos < 0 ORDER BY pos DESC";
+	query << "SELECT equipid, type, pos FROM equip WHERE charid = " << mysqlpp::quote << id << " AND pos < 0 ORDER BY type ASC, pos ASC";
 	mysqlpp::StoreQueryResult res = query.store();
 
 	for (size_t i = 0; i < res.num_rows(); ++i) {
