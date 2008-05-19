bbc6ddcaa8f8410fbdc39432ad32071b2b00d355
pawitp <pawitp@23f3cdc0-e6d1-42be-a0c1-51f4125517bb>
2008-05-19 04:20:58 +0000

    More NPC exports
    git-svn-id: https://dev.chiasoft.net:8443/svn/svn_vana/trunk@316 23f3cdc0-e6d1-42be-a0c1-51f4125517bb

--- src/ChannelServer/LuaNPC.cpp
+++ src/ChannelServer/LuaNPC.cpp
@@ -19,6 +19,10 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include "LuaNPC.h"
 #include "Player.h"
 #include "Players.h"
+#include "Quests.h"
+#include "Maps.h"
+#include "Levels.h"
+#include "InventoryPacket.h"
 
 LuaNPC::LuaNPC(char *filename, NPC *npc) {
 	lua_State *luaVm = lua_open();
@@ -29,7 +33,17 @@ LuaNPC::LuaNPC(char *filename, NPC *npc) {
 	lua_setglobal(luaVm, "state");
 
 	lua_register(luaVm, "addText", &LuaNPCExports::addText);
+	lua_register(luaVm, "addChar", &LuaNPCExports::addChar);
 	lua_register(luaVm, "sendSimple", &LuaNPCExports::sendSimple);
+	lua_register(luaVm, "sendYesNo", &LuaNPCExports::sendYesNo);
+	lua_register(luaVm, "sendNext", &LuaNPCExports::sendNext);
+	lua_register(luaVm, "sendBackNext", &LuaNPCExports::sendBackNext);
+	lua_register(luaVm, "sendBackOK", &LuaNPCExports::sendBackOK);
+	lua_register(luaVm, "sendOK", &LuaNPCExports::sendOK);
+	lua_register(luaVm, "sendAcceptDecline", &LuaNPCExports::sendAcceptDecline);
+	lua_register(luaVm, "sendGetText", &LuaNPCExports::sendGetText);
+	lua_register(luaVm, "sendGetNumber", &LuaNPCExports::sendGetNumber);
+	lua_register(luaVm, "sendStyle", &LuaNPCExports::sendStyle);
 	lua_register(luaVm, "endNPC", &LuaNPCExports::end); // end() doesn't work (reserved?)
 
 	luaL_dofile(luaVm, filename);
@@ -46,8 +60,162 @@ int LuaNPCExports::addText(lua_State *luaVm) {
 	return 1;
 }
 
+int LuaNPCExports::addChar(lua_State *luaVm) {
+	getNPC(luaVm)->addChar((char) lua_tointeger(luaVm, -1));
+	return 1;
+}
+
 int LuaNPCExports::sendSimple(lua_State *luaVm) {
 	getNPC(luaVm)->sendSimple();
+	return 1;
+}
+	
+int LuaNPCExports::sendYesNo(lua_State *luaVm) {
+	getNPC(luaVm)->sendYesNo();
+	return 1;
+}
+
+int LuaNPCExports::sendNext(lua_State *luaVm) {
+	getNPC(luaVm)->sendNext();
+	return 1;
+}
+
+int LuaNPCExports::sendBackNext(lua_State *luaVm) {
+	getNPC(luaVm)->sendBackNext();
+	return 1;
+}
+
+int LuaNPCExports::sendBackOK(lua_State *luaVm) {
+	getNPC(luaVm)->sendBackOK();
+	return 1;
+}
+
+int LuaNPCExports::sendOK(lua_State *luaVm) {
+	getNPC(luaVm)->sendOK();
+	return 1;
+}
+
+int LuaNPCExports::sendAcceptDecline(lua_State *luaVm) {
+	getNPC(luaVm)->sendAcceptDecline();
+	return 1;
+}
+
+int LuaNPCExports::sendGetText(lua_State *luaVm) {
+	getNPC(luaVm)->sendGetText();
+	return 1;
+}
+
+int LuaNPCExports::sendGetNumber(lua_State *luaVm) {
+	getNPC(luaVm)->sendGetNumber(lua_tointeger(luaVm, -3), lua_tointeger(luaVm, -2), lua_tointeger(luaVm, -1));
+	return 1;
+}
+
+int LuaNPCExports::sendStyle(lua_State *luaVm) {
+	char size = (char) lua_tointeger(luaVm, -1);
+	int *styles = new int[size];
+
+	lua_pop(luaVm, 1);
+	lua_pushnil(luaVm);
+	while (lua_next(luaVm, -2) != 0) {
+		int a = lua_tointeger(luaVm, -2)-1;
+		int b = lua_tointeger(luaVm, -1);
+		styles[lua_tointeger(luaVm, -2)-1] = lua_tointeger(luaVm, -1);
+		lua_pop(luaVm, 1);
+	}
+
+	getNPC(luaVm)->sendStyle(styles, size);
+	return 1;
+}
+
+int LuaNPCExports::giveItem(lua_State *luaVm) {
+	Quests::giveItem(getNPC(luaVm)->getPlayer(), lua_tointeger(luaVm, -2), lua_tointeger(luaVm, -1));
+	return 1;
+}
+
+int LuaNPCExports::giveMesos(lua_State *luaVm) {
+	Quests::giveMesos(getNPC(luaVm)->getPlayer(), lua_tointeger(luaVm, -1));
+	return 1;
+}
+
+int LuaNPCExports::giveEXP(lua_State *luaVm) {
+	Levels::giveEXP(getNPC(luaVm)->getPlayer(), lua_tointeger(luaVm, -1), 1);
+	return 1;
+}
+
+int LuaNPCExports::getLevel(lua_State *luaVm) {
+	lua_pushnumber(luaVm, getNPC(luaVm)->getPlayer()->getLevel());
+	return 1;
+}
+
+int LuaNPCExports::getGender(lua_State *luaVm) {
+	lua_pushnumber(luaVm, getNPC(luaVm)->getPlayer()->getGender());
+	return 1;
+}
+
+int LuaNPCExports::getItemAmount(lua_State *luaVm) {
+	lua_pushnumber(luaVm, getNPC(luaVm)->getPlayer()->inv->getItemAmount(lua_tointeger(luaVm, -1)));
+	return 1;
+}
+
+int LuaNPCExports::getMesos(lua_State *luaVm) {
+	lua_pushnumber(luaVm, getNPC(luaVm)->getPlayer()->inv->getMesos());
+	return 1;
+}
+
+int LuaNPCExports::getMap(lua_State *luaVm) {
+	lua_pushnumber(luaVm, getNPC(luaVm)->getPlayer()->getMap());
+	return 1;
+}
+
+int LuaNPCExports::getHP(lua_State *luaVm) {
+	lua_pushnumber(luaVm, getNPC(luaVm)->getPlayer()->getHP());
+	return 1;
+}
+
+int LuaNPCExports::getHair(lua_State *luaVm) {
+	lua_pushnumber(luaVm, getNPC(luaVm)->getPlayer()->getHair());
+	return 1;
+}
+
+int LuaNPCExports::getEyes(lua_State *luaVm) {
+	lua_pushnumber(luaVm, getNPC(luaVm)->getPlayer()->getEyes());
+	return 1;
+}
+
+int LuaNPCExports::setStyle(lua_State *luaVm) {
+	int id = lua_tointeger(luaVm, -1);
+	if(id/10000 == 0){
+		getNPC(luaVm)->getPlayer()->setSkin((char)id);
+	}
+	else if(id/10000 == 2){
+		getNPC(luaVm)->getPlayer()->setEyes(id);
+	}
+	else if(id/10000 == 3){
+		getNPC(luaVm)->getPlayer()->setHair(id);
+	}
+	InventoryPacket::updatePlayer(getNPC(luaVm)->getPlayer());
+	return 1;
+}
+
+int LuaNPCExports::setMap(lua_State *luaVm) {
+	int mapid = lua_tointeger(luaVm, -1);
+	if(Maps::info.find(mapid) != Maps::info.end())
+		Maps::changeMap(getNPC(luaVm)->getPlayer(), mapid, 0);
+	return 1;
+}
+
+int LuaNPCExports::setHP(lua_State *luaVm) {
+	getNPC(luaVm)->getPlayer()->setHP(lua_tointeger(luaVm, -1));
+	return 1;
+}
+
+int LuaNPCExports::addQuest(lua_State *luaVm) {
+	getNPC(luaVm)->getPlayer()->quests->addQuest(lua_tointeger(luaVm, -1), getNPC(luaVm)->getNpcID());
+	return 1;
+}
+
+int LuaNPCExports::endQuest(lua_State *luaVm) {
+	getNPC(luaVm)->getPlayer()->quests->finishQuest(lua_tointeger(luaVm, -1), getNPC(luaVm)->getNpcID());
 	return 1;
 }
 
