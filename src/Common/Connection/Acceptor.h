37d6c7062a5868488e9d7f6ec847f250555017aa
pawitp <pawitp@09c1253f-3d09-8e40-9719-4320e56601b5>
2008-05-27 10:34:43 +0000

    More cleaning up
    git-svn-id: svn://70.113.44.214/VanaMirror/trunk@425 09c1253f-3d09-8e40-9719-4320e56601b5

--- src/Common/Connection/Acceptor.h
+++ src/Common/Connection/Acceptor.h
@@ -23,7 +23,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 class AbstractPlayerFactory;
 
-class Acceptor: public Selector::SelectHandler {
+class Acceptor : public Selector::SelectHandler {
 public:
 	Acceptor(short port, AbstractPlayerFactory *apf);
 	virtual void handle(int socket);
