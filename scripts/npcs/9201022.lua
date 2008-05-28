9684980725b366afdd14dbf4d595ca67c52bd167
pawitp <pawitp@09c1253f-3d09-8e40-9719-4320e56601b5>
2008-05-28 13:17:13 +0000

    Thomas Swift NPC (by Blader)
    git-svn-id: svn://70.113.44.214/VanaMirror/trunk@447 09c1253f-3d09-8e40-9719-4320e56601b5

---
+++ scripts/npcs/9201022.lua
@@ -0,0 +1,35 @@
+if getMap() == 100000000 then
+	if state == 0 then
+		addText("I can take you to Amoria Village. Are you ready to go?");
+		sendYesNo();
+	elseif state == 1 then
+		if getSelected() == 1 then
+			addText("I hope you had a great time! See you around!");
+			sendNext();
+		else
+			addText("Ok, feel free to hang around until you're ready to go!");
+			sendOK();
+			endNPC();
+		end
+	elseif state == 2 then
+		setMap(680000000);
+		endNPC();
+	end
+else	
+	if state == 0 then
+		addText("I can take you back to your original location. Are you ready to go?");
+		sendYesNo();
+	elseif state == 1 then
+		if getSelected() == 1 then
+			addText("I hope you had a great time! See you around!");
+			sendNext();
+		else
+			addText("Ok, feel free to hang around until you're ready to go!");
+			sendNext();
+			endNPC();
+		end
+	elseif state == 2 then
+		setMap(100000000);
+		endNPC();
+	end
+end
