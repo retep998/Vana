b58a3f7616b37efe7f6e42ff2c1089129055006b
doyos <doyos@23f3cdc0-e6d1-42be-a0c1-51f4125517bb>
2008-05-29 02:50:26 +0000

    Added portal scripts for Ellinia <-> Leafre, 4th job advancement map, and Origin of Clocktower
    git-svn-id: https://dev.chiasoft.net:8443/svn/svn_vana/trunk@454 23f3cdc0-e6d1-42be-a0c1-51f4125517bb

---
+++ scripts/portals/minar_elli.lua
@@ -0,0 +1,15 @@
+-- Ellinia <-> Leafre
+if getItemAmount(4031346) > 0 then
+	giveItem(4031346, -1);
+	showMessage("The Magical Seed is spent and you are transferred to somewhere.", 5);
+	if getMap() == 240010100 then
+		setPortalToId(101010000);
+		setPortalTo("minar00");
+	else
+		setPortalToId(240010100);
+		setPortalTo("elli00");
+	end
+else
+	showMessage("Magic Seed is needed to go through the portal.", 5);
+	setPortalToId(-1);
+end
