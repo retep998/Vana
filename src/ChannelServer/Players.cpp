5c58d2285e29601d325cf9f4d67b4c5c1ab95c2a
doyos <doyos@23f3cdc0-e6d1-42be-a0c1-51f4125517bb>
2008-05-30 04:46:07 +0000

    Fixed include issue in Players.cpp
    git-svn-id: https://dev.chiasoft.net:8443/svn/svn_vana/trunk@473 23f3cdc0-e6d1-42be-a0c1-51f4125517bb

--- src/ChannelServer/Players.cpp
+++ src/ChannelServer/Players.cpp
@@ -16,8 +16,9 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 #include "Players.h"
-#include "Player.h"
 #include "PlayersPacket.h"
+#include "Player.h"
+#include "PlayerPacket.h"
 #include "Maps.h" 
 #include "Mobs.h"
 #include "Shops.h"
