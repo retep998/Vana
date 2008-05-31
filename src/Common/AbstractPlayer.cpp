3b0fd0552ae26b3ab8c8d4817b195b416161ca6b
pawitp <pawitp@09c1253f-3d09-8e40-9719-4320e56601b5>
2008-05-31 10:57:59 +0000

    Fixed include problem in AbstractPlayer
    git-svn-id: svn://70.113.44.214/VanaMirror/trunk@493 09c1253f-3d09-8e40-9719-4320e56601b5

--- src/Common/AbstractPlayer.cpp
+++ src/Common/AbstractPlayer.cpp
@@ -16,7 +16,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 #include "AbstractPlayer.h"
-#include "Connection/PacketHandler.h"
+#include "PacketHandler.h"
 #include "PingPacket.h"
 #include "Timer.h"
 #include "BufferUtilities.h"
