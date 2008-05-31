a0a923e6a772f04ba74723fbd2e39622d0acb723
pawitp <pawitp@09c1253f-3d09-8e40-9719-4320e56601b5>
2008-05-31 00:09:20 +0000

    save_on_dc is now defaulted to true
    git-svn-id: svn://70.113.44.214/VanaMirror/trunk@482 09c1253f-3d09-8e40-9719-4320e56601b5

--- src/ChannelServer/Player.h
+++ src/ChannelServer/Player.h
@@ -44,7 +44,7 @@ struct SkillMapEnterActiveInfo;
 
 class Player : public AbstractPlayer {
 public:
-	Player() : isconnect(false), save_on_dc(false), shop(0), npc(0), chair(0), itemEffect(0) {
+	Player() : isconnect(false), save_on_dc(true), shop(0), npc(0), chair(0), itemEffect(0) {
 		channel = ChannelServer::Instance()->getChannel();
 		skill.types[0] = 0;
 		skill.types[1] = 0;
