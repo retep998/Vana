bacc2c26e577b5e13424049a785e3d1e0f837f7a
pawitp <pawitp@09c1253f-3d09-8e40-9719-4320e56601b5>
2008-06-01 13:29:15 +0000

    Removed unused CharUtilities
    git-svn-id: svn://70.113.44.214/VanaMirror/trunk@516 09c1253f-3d09-8e40-9719-4320e56601b5

--- src/ChannelServer/Players.cpp
+++ src/ChannelServer/Players.cpp
@@ -30,7 +30,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include "MapPacket.h"
 #include "PacketCreator.h"
 #include "BufferUtilities.h"
-#include "CharUtilities.h"
 #include "WorldServerConnectPlayerPacket.h"
 
 hash_map <int, Player*> Players::players;
