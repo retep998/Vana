3df8f579f84bf8366ff5586a2e6102e92294781f
doyos <doyos@23f3cdc0-e6d1-42be-a0c1-51f4125517bb>
2008-06-07 02:56:49 +0000

    Added reactor scripts for Hongbu and Nolbu's gourds, along with Zakum party quest chest and phoenix/frostprey reactors. Most by Robtbs
    git-svn-id: https://dev.chiasoft.net:8443/svn/svn_vana/trunk@563 23f3cdc0-e6d1-42be-a0c1-51f4125517bb

---
+++ scripts/reactors/2111000.lua
@@ -0,0 +1,5 @@
+-- Zakum Party Quest Chest - summons 3 "Mimics"
+showMessage("Oh noes! Monsters in the chest!", 5);
+spawnMob(9300004);
+spawnMob(9300004);
+spawnMob(9300004);
---
+++ scripts/reactors/2401002.lua
@@ -0,0 +1,2 @@
+-- Leafre Reactor - Egg for Frostprey Quest (4th job Marksman)
+spawnMob(9300090); 
