8fed111136ecfa49f1de3622a15e0b2961f16cbb
doyos <doyos@09c1253f-3d09-8e40-9719-4320e56601b5>
2008-06-18 01:19:33 +0000

    Re-enabled checking of reactors when dropping equips for protector rock reactor in GPQ area.
    git-svn-id: svn://70.113.44.214/VanaMirror/trunk@640 09c1253f-3d09-8e40-9719-4320e56601b5

--- src/ChannelServer/Inventory.cpp
+++ src/ChannelServer/Inventory.cpp
@@ -131,6 +131,7 @@ void Inventory::itemMove(Player *player, ReadPacket *packet) {
 			dropper.pos = drop->getPos();
 			drop->doDrop(dropper);
 			player->inv->deleteEquip(num);
+			Reactors::checkDrop(player, drop);
 			return;
 		}
 		for (int i = 0; i < player->inv->getEquipNum(); i++) {
