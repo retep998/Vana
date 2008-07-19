4cc1134ffbd37bb40fbe2aa01f74922d97e7777d
pawitp <pawitp@09c1253f-3d09-8e40-9719-4320e56601b5>
2008-07-19 03:18:37 +0000

    Rearranged headers in RecvHeader.h and SendHeader.h
    git-svn-id: svn://70.113.44.214/VanaMirror/trunk@798 09c1253f-3d09-8e40-9719-4320e56601b5

--- src/Common/RecvHeader.h
+++ src/Common/RecvHeader.h
@@ -20,59 +20,59 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 //Login
 #define RECV_LOGIN_INFO 0x01
+#define RECV_SHOW_WORLD2 0x04 // Click back after select channel
 #define RECV_CHANNEL_SELECT 0x05
 #define RECV_WORLD_SELECT 0x06
+#define RECV_SET_GENDER 0x08
 #define RECV_LOGIN_PROCESS 0x09
+#define RECV_REGISTER_PIN 0x0a
 #define RECV_SHOW_WORLD 0x0b
-#define RECV_SHOW_WORLD2 0x04 // Click back after select channel
 #define RECV_GET_CHANNEL_SERVER_INFO 0x13
 #define RECV_CHECK_CHAR_NAME 0x15
 #define RECV_CREATE_CHAR 0x16
 #define RECV_DELETE_CHAR 0x17
-#define RECV_SET_GENDER 0x08
-#define RECV_REGISTER_PIN 0x0a
 
 //Channel
 #define RECV_CHANNEL_LOGIN 0x14
-#define RECV_NPC_TALK_CONT 0x38 // i.e. clicking next on NPC
-#define RECV_SHOP_ENTER 0x39
-#define RECV_NPC_TALK 0x36
-#define RECV_CHANGE_CHANNEL 0x24
-#define RECV_DAMAGE_PLAYER 0x2d
-#define RECV_STOP_CHAIR 0x27
-#define RECV_CHAT 0x2e
-#define RECV_USE_CHAIR 0x28
-#define RECV_DAMAGE_MOB_SPELL 0x2b
 #define RECV_CHANGE_MAP 0x23
-#define RECV_PARTY_ACTION 0x6f
+#define RECV_CHANGE_CHANNEL 0x24
 #define RECV_MOVE_PLAYER 0x26
-#define RECV_DAMAGE_MOB_RANGED 0x2a
-#define RECV_GROUP_CHAT 0x6a // Party, buddy and guild chat
-#define RECV_GET_PLAYER_INFO 0x59
-#define RECV_CHANGE_MAP_SPECIAL 0x5c
-#define RECV_SPECIAL_SKILL 0x55 // Chakra, Pierce, Monster Magnet, Big Bang
-#define RECV_CANCEL_ITEM 0x44
-#define RECV_USE_SUMMON_BAG 0x46
-#define RECV_ADD_SKILL 0x52
-#define RECV_CANCEL_SKILL 0x54
-#define RECV_USE_SKILL 0x53
-#define RECV_USE_CASH_ITEM 0x49
-#define RECV_USE_SKILLBOOK 0x4b
-#define RECV_COMMAND 0x6b // / commands like "/find"
+#define RECV_STOP_CHAIR 0x27
+#define RECV_USE_CHAIR 0x28
 #define RECV_DAMAGE_MOB 0x29
+#define RECV_DAMAGE_MOB_RANGED 0x2a
+#define RECV_DAMAGE_MOB_SPELL 0x2b
+#define RECV_DAMAGE_PLAYER 0x2d
+#define RECV_CHAT 0x2e
 #define RECV_FACE_EXPERIMENT 0x30
 #define RECV_USE_ITEM_EFFECT 0x31
+#define RECV_NPC_TALK 0x36
+#define RECV_NPC_TALK_CONT 0x38 // i.e. clicking next on NPC
+#define RECV_SHOP_ENTER 0x39
 #define RECV_MOVE_ITEM 0x42
 #define RECV_USE_ITEM 0x43
+#define RECV_CANCEL_ITEM 0x44
+#define RECV_USE_SUMMON_BAG 0x46
+#define RECV_USE_CASH_ITEM 0x49
+#define RECV_USE_SKILLBOOK 0x4b
 #define RECV_USE_RETURN_SCROLL 0x4e
 #define RECV_USE_SCROLL 0x4f
 #define RECV_ADD_STAT 0x50
 #define RECV_HEAL_PLAYER 0x51
+#define RECV_ADD_SKILL 0x52
+#define RECV_USE_SKILL 0x53
+#define RECV_CANCEL_SKILL 0x54
+#define RECV_SPECIAL_SKILL 0x55 // Chakra, Pierce, Monster Magnet, Big Bang
 #define RECV_DROP_MESO 0x56
 #define RECV_FAME 0x57
+#define RECV_GET_PLAYER_INFO 0x59
+#define RECV_CHANGE_MAP_SPECIAL 0x5c
 #define RECV_GET_QUEST 0x62
+#define RECV_SKILL_MACRO 0x65
+#define RECV_GROUP_CHAT 0x6a // Party, buddy and guild chat
+#define RECV_COMMAND 0x6b // / commands like "/find"
+#define RECV_PARTY_ACTION 0x6f
 #define RECV_KEYMAP 0x7a
 #define RECV_LOOT_ITEM 0xa8
 #define RECV_HIT_REACTOR 0xab
 #define RECV_CONTROL_MOB 0x9a
-#define RECV_SKILL_MACRO 0x65
--- src/Common/SendHeader.h
+++ src/Common/SendHeader.h
@@ -22,79 +22,79 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 // Login
 #define SEND_LOGIN_INFO_REPLY 0x00
+#define SEND_SHOW_CHANNEL 0x03
+#define SEND_GENDER_DONE 0x04
 #define SEND_LOGIN_PROCESS 0x06
 #define SEND_PIN_ASSIGNED 0x07
-#define SEND_GENDER_DONE 0x04
 #define SEND_SHOW_WORLD 0x0a
-#define SEND_SHOW_CHANNEL 0x03
-#define SEND_CHANNEL_SELECT 0x14
 #define SEND_SHOW_CHARACTERS 0x0b
-#define SEND_SHOW_CHARACTER 0x0e
-#define SEND_CHECK_NAME 0x0d
-#define SEND_DELETE_CHAR 0x0f
 #define SEND_CHANNEL_SERVER_INFO 0x0c
+#define SEND_CHECK_NAME 0x0d
+#define SEND_SHOW_CHARACTER 0x0e
+#define SEND_DELETE_CHAR 0x0f
+#define SEND_CHANNEL_SELECT 0x14
 
 // Channel
 #define SEND_CHANGE_CHANNEL 0x10
 #define SEND_MOVE_ITEM 0x1a
-#define SEND_SHOW_MESSENGER 0x54 // Also known as avatar megaphone
-#define SEND_FINISH_QUEST 0x2e
 #define SEND_UPDATE_STAT 0x1c
+#define SEND_USE_SKILL 0x1d
 #define SEND_CANCEL_SKILL 0x1e
-#define SEND_PLAYER_INFO 0x3a
 #define SEND_ADD_SKILL 0x21
-#define SEND_NOTICE 0x41
 #define SEND_FAME 0x23
 #define SEND_NOTE 0x24 // Bottom-right corner notice
+#define SEND_FINISH_QUEST 0x2e
 #define SEND_USE_SKILLBOOK 0x30 // Skillbooks
+#define SEND_SHOW_ITEM_EFFECT 0x31
+#define SEND_PLAYER_INFO 0x3a
 #define SEND_PARTY_ACTION 0x3b
-#define SEND_USE_SKILL 0x1d
+#define SEND_NOTICE 0x41
+#define SEND_SHOW_MESSENGER 0x54 // Also known as avatar megaphone
+#define SEND_SKILL_MACRO 0x5a
 #define SEND_CHANGE_MAP 0x5b
+#define SEND_MAKE_APPLE 0x62 // Wear apple suit
+#define SEND_GROUP_CHAT 0x63 // Groups like party, buddies and guild
+#define SEND_COMMAND_RESPOND 0x64 // Respond to / commands i.e. "/find"
 #define SEND_MAP_EFFECT 0x67 // Change Music/Boss HP Bars
 #define SEND_USE_WEATHER 0x68 // Weather effects
-#define SEND_GROUP_CHAT 0x63 // Groups like party, buddies and guild
-#define SEND_MAKE_APPLE 0x62 // Wear apple suit
-#define SEND_COMMAND_RESPOND 0x64 // Respond to / commands i.e. "/find"
 #define SEND_TIME 0x6d // For places with clock i.e. ship station
 #define SEND_SHOW_PLAYER 0x77
-#define SEND_STOP_CHAIR 0x9f
-#define SEND_GAIN_ITEM 0xa0 // Gain item notice in chat
-#define SEND_UPDATE_QUEST 0xa5
 #define SEND_REMOVE_PLAYER 0x78
 #define SEND_CHAT 0x79
 #define SEND_USE_SCROLL 0x7d
 #define SEND_MOVE_PLAYER 0x8c
-#define SEND_SHOW_SKILL 0x98
-#define SEND_SHOW_OTHERS_SKILL 0x99 // From other player
 #define SEND_DAMAGE_MOB 0x8d
-#define SEND_CANCEL_OTHERS_BUFF 0x9a // From other player
-#define SEND_FACE_EXPERIMENT 0x94
 #define SEND_DAMAGE_MOB_RANGED 0x8e
-#define SEND_SHOW_ITEM_EFFECT 0x31
-#define SEND_DAMAGE_PLAYER 0x93
-#define SEND_SPECIAL_SKILL_END 0x92
+#define SEND_DAMAGE_MOB_SPELL 0x8f
 #define SEND_SPECIAL_SKILL 0x91
+#define SEND_SHOW_MAGNET 0x91
+#define SEND_SPECIAL_SKILL_END 0x92
+#define SEND_DAMAGE_PLAYER 0x93
+#define SEND_FACE_EXPERIMENT 0x94
 #define SEND_SIT_CHAIR 0x96
 #define SEND_UPDATE_CHAR_LOOK 0x97
-#define SEND_DAMAGE_MOB_SPELL 0x8f
+#define SEND_SHOW_SKILL 0x98
+#define SEND_SHOW_OTHERS_SKILL 0x99 // From other player
+#define SEND_CANCEL_OTHERS_BUFF 0x9a // From other player
+#define SEND_STOP_CHAIR 0x9f
+#define SEND_GAIN_ITEM 0xa0 // Gain item notice in chat
+#define SEND_UPDATE_QUEST 0xa5
+#define SEND_INSTRUCTION_BUBBLE 0xa8
 #define SEND_SHOW_MOB 0xad
-#define SEND_MOVE_MOB 0xb0
-#define SEND_SHOW_MOB_HP 0xbb
-#define SEND_SHOW_MAGNET 0x91
-#define SEND_SHOW_DRAGGED 0xbc // New in .56, Monster Magnet'd mobs get their own packet
-#define SEND_MOVE_MOB_RESPONSE 0xb1
-#define SEND_CONTROL_MOB 0xaf
 #define SEND_KILL_MOB 0xae
+#define SEND_CONTROL_MOB 0xaf
+#define SEND_MOVE_MOB 0xb0
+#define SEND_MOVE_MOB_RESPONSE 0xb1
+#define SEND_SHOW_MOB_HP 0xbb
+#define SEND_SHOW_DRAGGED 0xbc // New in .56, Monster Magnet'd mobs get their own packet
 #define SEND_SHOW_NPC 0xc0
 #define SEND_SHOW_NPC2 0xc2
-#define SEND_SHOW_REACTOR 0xd6
-#define SEND_TRIGGER_REACTOR 0xd4
-#define SEND_DESTROY_REACTOR 0xd7
 #define SEND_DROP_ITEM 0xcb
 #define SEND_TAKE_DROP 0xcc
+#define SEND_TRIGGER_REACTOR 0xd4
+#define SEND_SHOW_REACTOR 0xd6
+#define SEND_DESTROY_REACTOR 0xd7
 #define SEND_NPC_TALK 0xea
 #define SEND_SHOP_OPEN 0xeb
 #define SEND_SHOP_BOUGHT 0xec
 #define SEND_KEYMAP 0x104
-#define SEND_INSTRUCTION_BUBBLE 0xa8
-#define SEND_SKILL_MACRO 0x5a
