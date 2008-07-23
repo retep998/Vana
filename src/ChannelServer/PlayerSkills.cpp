680e9cce7f9569abbcba8a16f8830206939b144f
pawitp <pawitp@09c1253f-3d09-8e40-9719-4320e56601b5>
2008-07-23 13:14:01 +0000

    Moved PlayerSkills to its own file
    git-svn-id: svn://70.113.44.214/VanaMirror/trunk@845 09c1253f-3d09-8e40-9719-4320e56601b5

---
+++ src/ChannelServer/PlayerSkills.cpp
@@ -0,0 +1,147 @@
+/*
+Copyright (C) 2008 Vana Development Team
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; version 2
+of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#include "PlayerSkills.h"
+#include "Skills.h"
+
+void PlayerSkills::addSkillLevel(int skillid, int level, bool sendpacket) {
+	if (playerskills.find(skillid) != playerskills.end()) {
+		playerskills[skillid] += level;
+	}
+	else {
+		playerskills[skillid] = level;
+	}
+
+	if (sendpacket) {
+		int maxlevel = 0;
+		if (FORTHJOB_SKILL(skillid)) {
+			maxlevel = getMaxSkillLevel(skillid);
+		}
+		SkillsPacket::addSkill(player, skillid, getSkillLevel(skillid), maxlevel);
+	}
+}
+
+void PlayerSkills::deleteSkillMapEnterInfo(int skillid) {
+	for (size_t i= 0; i<activemapenterskill.size(); i++) {
+		if (activemapenterskill[i].skill == skillid) {
+			activemapenterskill.erase(activemapenterskill.begin()+i);
+		}
+	}
+}
+
+int PlayerSkills::getSkillLevel(int skillid) {
+	if (playerskills.find(skillid) != playerskills.end()) {
+		return playerskills[skillid];
+	}
+	else {
+		return 0;
+	}
+}
+
+int PlayerSkills::getMaxSkillLevel(int skillid) {
+	// Get max level for 4th job skills
+	if (maxlevels.find(skillid) != maxlevels.end()) {
+		return maxlevels[skillid];
+	}
+	else {
+		return 0;
+	}
+}
+
+int PlayerSkills::getSkillID(size_t i) {
+	size_t j = 0;
+	for (hash_map<int, int>::iterator iter = playerskills.begin(); iter != playerskills.end(); iter++) {
+		if (j == i) {
+			return iter->first;
+		} 
+		j++;
+	}
+	return 0;
+}
+
+SkillActiveInfo PlayerSkills::getSkillMapInfo(int skillid) {
+	return activemapskill[skillid];
+}
+
+SkillMapEnterActiveInfo PlayerSkills::getSkillMapEnterInfo() {
+	SkillMapEnterActiveInfo skill;
+	for (size_t i= 0; i<activemapenterskill.size(); i++) {
+		skill.types[activemapenterskill[i].byte-1] += activemapenterskill[i].type;
+		if (activemapenterskill[i].isvalue) {
+			skill.val = activemapenterskill[i].value;
+			skill.isval = true;
+		}
+	}
+	return skill;
+}
+
+SkillActiveInfo PlayerSkills::getSkillPlayerInfo(int skillid) {
+	return activeplayerskill[skillid];
+}
+
+int PlayerSkills::getActiveSkillLevel(int skillid) {
+	if (activelevels.find(skillid) == activelevels.end()) {
+		return 0;
+	}
+	return activelevels[skillid];
+}
+
+size_t PlayerSkills::getActiveSkillsNum() {
+	size_t j = 0;
+	for (hash_map<int, int>::iterator iter = activelevels.begin(); iter != activelevels.end(); iter++) {
+		j++;
+	}
+	return j;
+}
+
+int PlayerSkills::getActiveSkillsID(size_t i) {
+	size_t j = 0;
+	for (hash_map<int, int>::iterator iter = activelevels.begin(); iter != activelevels.end(); iter++) {
+		if (j == i) {
+			return iter->first;
+		} 
+		j++;
+	}
+	return 0;
+}
+
+void PlayerSkills::setSkillMapEnterInfo(int skillid, vector <SkillMapActiveInfo> skill) {
+	// TEMP //
+	for (unsigned int i=0; i<activemapenterskill.size(); i++) { 
+		if (activemapenterskill[i].isvalue) {
+			activemapenterskill.erase(activemapenterskill.begin()+i);
+			break;
+		}
+	}
+	//////////
+	for (unsigned int i=0; i<skill.size(); i++) {
+		activemapenterskill.push_back(skill[i]);
+	}
+}
+
+void PlayerSkills::setSkillPlayerInfo(int skillid, SkillActiveInfo skill) {
+	activeplayerskill[skillid] = skill;
+}
+
+void PlayerSkills::setSkillMapInfo(int skillid, SkillActiveInfo skill) {
+	activemapskill[skillid] = skill;
+}
+
+void PlayerSkills::setActiveSkillLevel(int skillid, int level) {
+	activelevels[skillid] = level;
+}
