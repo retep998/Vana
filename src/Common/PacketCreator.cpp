96714da7a5829e79067784ec2525e57966396d6b
pawitp <pawitp@09c1253f-3d09-8e40-9719-4320e56601b5>
2008-07-23 12:21:26 +0000

    - De-inlined many complex PacketCreator functions  - Make sure PacketCreator accepts lengths as unsigned
    git-svn-id: svn://70.113.44.214/VanaMirror/trunk@841 09c1253f-3d09-8e40-9719-4320e56601b5

--- src/Common/PacketCreator.cpp
+++ src/Common/PacketCreator.cpp
@@ -30,6 +30,11 @@ void Packet::addBuffer(ReadPacket *packet) {
 	addBuffer(packet->getBuffer(), packet->getBufferLength());
 }
 
+void Packet::addBuffer(unsigned char *bytes, size_t len){
+	memcpy_s(packet+pos, len, bytes, len);
+	pos += len;
+}
+
 void Packet::addIP(const string &ip) { 
 	typedef boost::tokenizer<boost::char_separator<char>> tokenizer;
 	typedef boost::char_separator<char> separator;
@@ -40,4 +45,53 @@ void Packet::addIP(const string &ip) {
 	for (tokenizer::iterator iter = tokens.begin(); iter != tokens.end(); iter++) {
 		addByte((unsigned char) StringUtilities::toType<short>(*iter)); // Using toType<unsigned char> would cause it to spit ASCII
 	}
+}
+
+void Packet::addBytes(char *hex) {
+	for (size_t i = 0; i< strlen(hex)/2; i++) {
+		unsigned char byte1 = hex[i*2];
+		unsigned char byte2 = hex[i*2+1];
+		if (byte1 >= 'A' && byte1 <= 'F')
+			byte1 -= 'A' - 0xa;
+		else if (byte1 >= 'a' && byte1 <= 'f')
+			byte1 -= 'a' - 0xa;
+		else if (byte1 >= '0' && byte1 <= '9')
+			byte1 -= '0';
+		if (byte2 >= 'A' && byte2 <= 'F')
+			byte2 -= 'A' - 0xa;
+		else if (byte2 >= 'a' && byte2 <= 'f')
+			byte2 -= 'a' - 0xa;
+		else if (byte2 >= '0' && byte2 <= '9')
+			byte2 -= '0';
+		unsigned char byte = byte1*0x10 + byte2;
+		packet[pos++] = byte;	
+	}
+}
+
+void Packet::addString(const string &str, size_t len) {
+	size_t slen = str.size();
+	if (len < slen) {
+		std::cout << "ERROR: addString used with length shorter than string size." << std::endl; // TODO: Throw exception
+	}
+	strncpy_s((char *) packet + pos, MAX_LEN -pos, str.c_str(), slen);
+	for (int i = slen; i < len; i++) {
+		packet[pos+i] = 0;
+	}
+	pos += len;
+}
+
+void Packet::addString(const char *str, size_t slen) {
+	size_t rlen = strlen(str);
+	strncpy_s((char*)packet+pos, slen+1, str, slen);
+	for (size_t i = rlen; i < slen; i++) {
+		packet[pos+i] = 0;
+	}
+	pos += slen;
+}
+
+void Packet::addString(const string &str) {
+	size_t len = str.size();
+	addShort(len);
+	strcpy_s((char *) packet + pos, MAX_LEN - pos, str.c_str());
+	pos += len;
 }
