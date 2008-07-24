3555bcb54a42286325eb1f45934711f97ee4421d
pawitp <pawitp@23f3cdc0-e6d1-42be-a0c1-51f4125517bb>
2008-07-24 09:27:59 +0000

    Moved PlayerQuests into its own file
    git-svn-id: https://dev.chiasoft.net:8443/svn/svn_vana/trunk@882 23f3cdc0-e6d1-42be-a0c1-51f4125517bb

--- src/ChannelServer/Player.h
+++ src/ChannelServer/Player.h
@@ -22,6 +22,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include "PacketHandler.h"
 #include "PlayerInventory.h"
 #include "PlayerSkills.h"
+#include "PlayerQuests.h"
 #include "Skills.h"
 #include "Quests.h"
 #include "ChannelServer.h"
