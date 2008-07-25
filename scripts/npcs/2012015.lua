05ec3f03e4d9649f111c0119369e6850522a5faf
pawitp <pawitp@23f3cdc0-e6d1-42be-a0c1-51f4125517bb>
2008-07-25 09:13:48 +0000

    Fixed orbis tower/dolphin NPCs
    git-svn-id: https://dev.chiasoft.net:8443/svn/svn_vana/trunk@901 23f3cdc0-e6d1-42be-a0c1-51f4125517bb

--- scripts/npcs/2012015.lua
+++ scripts/npcs/2012015.lua
@@ -1,23 +1,18 @@
 -- El Nath Magic Spot (Orbis Tower: 1st Floor)
 if getItemAmount(4001019) >= 1 then
 	if state == 0 then
-		addText("You can use #bOrbis Rock Scroll#k to activate #bEl Nath Magic Spot.#k ");
-		addText("Will you teleport to where #bOrbis Magic Spot#k is?");
+		addText("You can use #b#t4001019##k to activate #b#p2012015##k. Will you teleport to where #b#p2012014##k is?");
 		sendYesNo();
 	elseif state == 1 then
 		if getSelected() == 1 then
 			giveItem(4001019, -1);
 			setMap(200080200);
-			endNPC();
-		else
-			endNPC();
 		end
+		endNPC();
 	end
-elseif getItemAmount(4001019) == 0 then
+else
 	if state == 0 then
-		addText("There's a #bEl Nath Magic Spot#k that'll enable you to teleport ");
-		addText("to where #bOrbis Magic Spot#k is, but you can't activate ");
-		addText("it without the scroll.");
+		addText("There's a #b#p2012015##k that'll enable you to teleport to where #b#p2012014##k is, but you can't activate it without the scroll.");
 		sendOK();
 		endNPC();
 	end
