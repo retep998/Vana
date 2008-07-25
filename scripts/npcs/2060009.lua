02dd72ee6c772879e49843db6fde63f6c96b01f0
pawitp <pawitp@09c1253f-3d09-8e40-9719-4320e56601b5>
2008-07-25 09:13:48 +0000

    Fixed orbis tower/dolphin NPCs
    git-svn-id: svn://70.113.44.214/VanaMirror/trunk@901 09c1253f-3d09-8e40-9719-4320e56601b5

--- scripts/npcs/2060009.lua
+++ scripts/npcs/2060009.lua
@@ -1,25 +1,22 @@
 -- Dolphin (Aquarium)
--- By Strife of KStory + Optimised/Cleaned up by Doyos of Vana
 
 prices = {1000, 10000};
 maps = {230030200, 251000100};
+
 if state == 0 then
-	addText("Oceans are all connected to each other. ");
-	addText("Places you can't reach by foot can be easily reached oversea. ");
-	addText("How about taking #bDolphin Taxi#k with us today?");
-	addText("\r\n#L1#Go to #bThe Sharp Unknown#k after paying 1000 mesos.#l");
-	addText("\r\n#L2#Go to #bHerb Town#k after paying 10000 mesos.#l");
+	addText("Oceans are all connected to each other. Places you can't reach by foot can be easily reached oversea. How about taking #bDolphin Taxi#k with us today? \r\n");
+	addText("#L0# Go to #bThe Sharp Unknown#k after paying 1000mesos.#l\r\n");
+	addText("#L1# Go to #bHerb Town#k after paying 10000mesos.#l");
 	sendSimple();
 elseif state == 1 then
-	where = getSelected();
+	where = getSelected() + 1;
 	if getMesos() >= prices[where] then -- Go to Sharp Unknown/Herb Town
 		giveMesos(-prices[where]);
 		setMap(maps[where]);
 		endNPC();
 	else
 		addText("I don't think you have enough money...");
-		sendOK();
-		endNPC();
+		sendNext();
 	end
 else
 	endNPC();
