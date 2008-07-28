12bbbd6287546576cc01b10810fdc4cce2528917
LazyBui <LazyBui@23f3cdc0-e6d1-42be-a0c1-51f4125517bb>
2008-07-28 20:08:12 +0000

    - Added buff type enum
    git-svn-id: https://dev.chiasoft.net:8443/svn/svn_vana/trunk@944 23f3cdc0-e6d1-42be-a0c1-51f4125517bb

--- src/ChannelServer/Skills.h
+++ src/ChannelServer/Skills.h
@@ -71,6 +71,17 @@ enum {
 	SKILL_LV
 };
 
+enum {
+	TYPE_5 = 1,
+	TYPE_6,
+	TYPE_7,
+	TYPE_8,
+	TYPE_1,
+	TYPE_2,
+	TYPE_3,
+	TYPE_4
+};
+
 struct SkillPlayerInfo {
 	unsigned char type;
 	char byte;
