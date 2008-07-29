f9c0d87108a4ad31d56511709390f0b8a8649426
doyos <doyos@09c1253f-3d09-8e40-9719-4320e56601b5>
2008-07-29 02:52:42 +0000

    Changed InitializeChannel.h to expect VEDB 14.
    git-svn-id: svn://70.113.44.214/VanaMirror/trunk@947 09c1253f-3d09-8e40-9719-4320e56601b5

--- src/ChannelServer/InitializeChannel.h
+++ src/ChannelServer/InitializeChannel.h
@@ -30,7 +30,7 @@ namespace Initializing {
 	void initializeQuests();
 	void initializeSkills();
 
-	const int vedb_version = 13;
+	const int vedb_version = 14;
 	const int vedb_subversion = 0;
 };
 
