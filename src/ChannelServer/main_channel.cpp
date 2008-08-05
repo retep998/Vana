55aae9272444f95b98cdd1c87064a8b641ac6ea2
doyos <doyos@09c1253f-3d09-8e40-9719-4320e56601b5>
2008-08-05 23:08:55 +0000

    Cleaned up some whitespaces.
    git-svn-id: svn://70.113.44.214/VanaMirror/trunk@1018 09c1253f-3d09-8e40-9719-4320e56601b5

--- src/ChannelServer/main_channel.cpp
+++ src/ChannelServer/main_channel.cpp
@@ -20,7 +20,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 int main() {
 	ChannelServer *server = ChannelServer::Instance();
 	server->initialize();
-	
+
 	while (getchar()) {}
 
 	server->shutdown();
