f6dd46aabfae2da5251363f1e2fbb090bd030199
pawitp <pawitp@23f3cdc0-e6d1-42be-a0c1-51f4125517bb>
2008-08-11 04:12:06 +0000

    Changed winsock include to winsock.h (reduces conflict with boost (include order sensitivity))
    git-svn-id: https://dev.chiasoft.net:8443/svn/svn_vana/trunk@1108 23f3cdc0-e6d1-42be-a0c1-51f4125517bb

--- src/Common/WinSockInclude.h
+++ src/Common/WinSockInclude.h
@@ -18,6 +18,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #ifndef WINSOCKINCLUDE_H
 #define WINSOCKINCLUDE_H
 
-#include <Winsock2.h>
+#include <Winsock.h>
 
 #endif
