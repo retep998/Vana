3f59249d1cbea796e3a3cd6e07a99284008e2f29
doyos <doyos@09c1253f-3d09-8e40-9719-4320e56601b5>
2008-08-14 01:25:00 +0000

    Further code simplification in PlayerInventory::addEquippedPacket
    git-svn-id: svn://70.113.44.214/VanaMirror/trunk@1150 09c1253f-3d09-8e40-9719-4320e56601b5

--- src/ChannelServer/PlayerInventory.cpp
+++ src/ChannelServer/PlayerInventory.cpp
@@ -147,13 +147,12 @@ void PlayerInventory::addEquipped(short slot, int itemid) {
 
 void PlayerInventory::addEquippedPacket(PacketCreator &packet) {
 	for (char i = 0; i < 50; i++) { // Shown items
-		if ((equipped[i][0] <= 0 && equipped[i][1] > 0) || (i == 11 && equipped[i][1] > 0)) { // Normal weapons always here
+		if (equipped[i][0] > 0 || equipped[i][1] > 0) {
 			packet.addByte(i);
-			packet.addInt(equipped[i][1]);
-		}
-		else if (equipped[i][0] > 0) {
-			packet.addByte(i);
-			packet.addInt(equipped[i][0]);
+			if (equipped[i][0] <= 0 || (i == 11 && equipped[i][1] > 0)) // Normal weapons always here
+				packet.addInt(equipped[i][1]);
+			else
+				packet.addInt(equipped[i][0]);
 		}
 	}
 	packet.addByte(-1);
