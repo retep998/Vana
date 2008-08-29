retep998/Vana

commit 854104a2a94097f34669d51675223b1f7361b87c

- Added name tracking in items (Blah's Blue Scarab)  - Added Item Tag code
git-svn-id: svn://70.113.44.214/VanaMirror/trunk@1226 09c1253f-3d09-8e40-9719-4320e56601b5

src/ChannelServer/PlayerInventory.cpp
@@ -263,7 +263,8 @@ void PlayerInventory::save() {
 				<< mysqlpp::quote << item->ihand << ","
 				<< mysqlpp::quote << item->ijump << ","
 				<< mysqlpp::quote << item->ispeed << ","
-				<< mysqlpp::quote << item->petid << ")";
+				<< mysqlpp::quote << item->petid << ","
+				<< mysqlpp::quote << item->name << ")";
 		}
 	}
 	query.exec();

src/ChannelServer/PlayerInventory.h
@@ -19,14 +19,16 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #define PLAYERINVENTORY_H
 
 #include <unordered_map>
+
 using std::tr1::unordered_map;
+using std::string;
 
 class Player;
 class PacketCreator;
 
 struct Item {
 	Item () : amount(1), slots(0), scrolls(0), istr(0), idex(0), iint(0), iluk(0), ihp(0),
-		imp(0), iwatk(0), imatk(0), iwdef(0), imdef(0), iacc(0), iavo(0), ihand(0), ijump(0), ispeed(0), petid(0) { }
+		imp(0), iwatk(0), imatk(0), iwdef(0), imdef(0), iacc(0), iavo(0), ihand(0), ijump(0), ispeed(0), petid(0), name("") { }
 	Item (int itemid, short amount) : id(itemid), amount(amount), petid(0) { }
 	Item (int equipid, bool random);
 	Item (Item *item) {
@@ -50,6 +52,7 @@ struct Item {
 		ijump = item->ijump;
 		ispeed = item->ispeed;
 		petid = item->petid;
+		name = item->name;
 	}
 	int id;
 	short amount;
@@ -71,6 +74,7 @@ struct Item {
 	short ijump;
 	short ispeed;
 	int petid;
+	string name;
 };
 typedef unordered_map<short, Item *> iteminventory;
 

sql/0045_add_item_name.sql
@@ -1,0 +1,1 @@
+ALTER TABLE items ADD name VARCHAR(12) NOT NULL AFTER petid;

src/ChannelServer/Inventory.cpp
@@ -698,6 +698,20 @@ void Inventory::useCashItem(Player *player, ReadPacket *packet) {
 			used = true;
 			break;
 		}
+		case 5060000: { // Item Name Tag
+			short slot = packet->getShort();
+			if (slot != 0) {
+				Item *item = player->getInventory()->getItem(1, slot);
+				if (item == 0) {
+					// Hacking or failure, dunno
+					return;
+				}
+				item->name = player->getName();
+				InventoryPacket::addNewItem(player, 1, slot, item, true);
+				used = true;
+			}
+			break;
+		}
 		default:
 			break;
 	}

src/ChannelServer/Player.cpp
@@ -186,7 +186,7 @@ void Player::playerConnect(ReadPacket *packet) {
 
 	inv->setMesosStart(res[0]["mesos"]);
 
-	query << "SELECT inv, slot, itemid, amount, slots, scrolls, istr, idex, iint, iluk, ihp, imp, iwatk, imatk, iwdef, imdef, iacc, iavo, ihand, ispeed, ijump, petid, pets.index, pets.name, pets.level, pets.closeness, pets.fullness FROM items LEFT JOIN pets ON items.petid=pets.id WHERE charid = " << mysqlpp::quote << getId();
+	query << "SELECT inv, slot, itemid, amount, slots, scrolls, istr, idex, iint, iluk, ihp, imp, iwatk, imatk, iwdef, imdef, iacc, iavo, ihand, ispeed, ijump, petid, items.name, pets.index, pets.name, pets.level, pets.closeness, pets.fullness FROM items LEFT JOIN pets ON items.petid=pets.id WHERE charid = " << mysqlpp::quote << getId();
 	res = query.store();
 
 	for (size_t i = 0; i < res.num_rows(); ++i) {
@@ -211,6 +211,7 @@ void Player::playerConnect(ReadPacket *packet) {
 		item->ijump = res[i][19];
 		item->ispeed = res[i][20];
 		item->petid = res[i][21];
+		res[i][22].to_string(item->name);
 		inv->addItem((unsigned char) res[i][0], res[i][1], item);
 		if (item->petid != 0) {
 			Pet *pet = new Pet(this);

src/ChannelServer/PlayerPacketHelper.cpp
@@ -33,7 +33,8 @@ void PlayerPacketHelper::addItemInfo(PacketCreator &packet, short slot, Item *it
 	}
 	packet.addByte(!ISEQUIP(item->id) + 1);
 	packet.addInt(item->id);
-	packet.addShort(0);
+	packet.addByte(0);
+	packet.addByte(0);
 	packet.addBytes("8005BB46E61702");
 	if (ISEQUIP(item->id)) {
 		packet.addByte(item->slots); // Slots
@@ -53,12 +54,10 @@ void PlayerPacketHelper::addItemInfo(PacketCreator &packet, short slot, Item *it
 		packet.addShort(item->ihand); // Hands
 		packet.addShort(item->ispeed); // Speed
 		packet.addShort(item->ijump); // Jump
-		packet.addShort(0); // Owner string goes here
-		packet.addShort(0);
-		packet.addShort(0);
-		packet.addShort(0);
-		packet.addShort(0);
-		packet.addShort(0);
+		packet.addString(item->name); // Owner string
+		packet.addByte(0); // Lock
+		packet.addInt64(0); // Expiration time
+		packet.addByte(0); // No clue
 	}
 	else {
 		packet.addShort(item->amount); // Amount

commit 83a311509f216d9dfbd150511938fc5ce6c4e1c3

- Fixed the character creation crash introduced in r1226  - Added a couple missing name defaults to the Item struct
git-svn-id: svn://70.113.44.214/VanaMirror/trunk@1229 09c1253f-3d09-8e40-9719-4320e56601b5

src/ChannelServer/PlayerInventory.cpp
@@ -25,7 +25,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include "MySQLM.h"
 
 /* Item struct */
-Item::Item(int equipid, bool random) : id(equipid), amount(1), scrolls(0), petid(0) {
+Item::Item(int equipid, bool random) : id(equipid), amount(1), scrolls(0), petid(0), name("") {
 	EquipInfo ei = Inventory::equips[equipid];
 	slots = ei.slots;
 	if (!random) {

src/ChannelServer/PlayerInventory.h
@@ -29,7 +29,7 @@ class PacketCreator;
 struct Item {
 	Item () : amount(1), slots(0), scrolls(0), istr(0), idex(0), iint(0), iluk(0), ihp(0),
 		imp(0), iwatk(0), imatk(0), iwdef(0), imdef(0), iacc(0), iavo(0), ihand(0), ijump(0), ispeed(0), petid(0), name("") { }
-	Item (int itemid, short amount) : id(itemid), amount(amount), petid(0) { }
+	Item (int itemid, short amount) : id(itemid), amount(amount), petid(0), name("") { }
 	Item (int equipid, bool random);
 	Item (Item *item) {
 		id = item->id;

src/LoginServer/Characters.cpp
@@ -88,10 +88,10 @@ void Characters::checkCharacterName(PlayerLogin *player, ReadPacket *packet) {
 void Characters::createEquip(int equipid, int type, int charid) {
 	mysqlpp::Query query = Database::chardb.query();
 	switch (type) {
-		case 0x05: query << "INSERT INTO items (charid, inv, slot, itemid, iwdef) VALUES (" << charid << "," << 1 << "," << -type << "," << equipid << "," << 3 << ")"; break;
-		case 0x06: query << "INSERT INTO items (charid, inv, slot, itemid, iwdef) VALUES (" << charid << "," << 1 << "," << -type << "," << equipid << "," << 2 << ")"; break;
-		case 0x07: query << "INSERT INTO items (charid, inv, slot, itemid, iwdef, slots) VALUES (" << charid << "," << 1 << "," << -type << "," << equipid << "," << 3 << "," << 5 << ")"; break;
-		case 0x0b: query << "INSERT INTO items (charid, inv, slot, itemid, iwatk) VALUES (" << charid << "," << 1 << "," << -type << "," << equipid << "," << 17 << ")"; break;
+		case 0x05: query << "INSERT INTO items (charid, inv, slot, itemid, iwdef, name) VALUES (" << charid << "," << 1 << "," << -type << "," << equipid << "," << 3 << ",\"\")"; break;
+		case 0x06: query << "INSERT INTO items (charid, inv, slot, itemid, iwdef, name) VALUES (" << charid << "," << 1 << "," << -type << "," << equipid << "," << 2 << ",\"\")"; break;
+		case 0x07: query << "INSERT INTO items (charid, inv, slot, itemid, iwdef, slots, name) VALUES (" << charid << "," << 1 << "," << -type << "," << equipid << "," << 3 << "," << 5 << ",\"\")"; break;
+		case 0x0b: query << "INSERT INTO items (charid, inv, slot, itemid, iwatk, name) VALUES (" << charid << "," << 1 << "," << -type << "," << equipid << "," << 17 << ",\"\")"; break;
 	}
 	query.exec();
 }
@@ -146,7 +146,7 @@ void Characters::createCharacter(PlayerLogin *player, ReadPacket *packet) {
 	createEquip(packet->getInt(), 0x07, id);
 	createEquip(packet->getInt(), 0x0b, id);
 
-	query << "INSERT INTO items (charid, inv, slot, itemid, amount) VALUES (" << mysqlpp::quote << id << ", 4, 1, 4161001, 1)"; // Beginner Guide
+	query << "INSERT INTO items (charid, inv, slot, itemid, amount, name) VALUES (" << mysqlpp::quote << id << ", 4, 1, 4161001, 1, \"\")"; // Beginner Guide
 	query.exec();
 
 	query << "SELECT * FROM characters WHERE id = " << mysqlpp::quote << id << " LIMIT 1";