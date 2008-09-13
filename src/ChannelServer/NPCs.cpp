9593bbfc34eb213a9a8e1458c935928d2b7a05d7
LazyBui <LazyBui@23f3cdc0-e6d1-42be-a0c1-51f4125517bb>
2008-09-13 20:19:33 +0000

    - Updated layout of the NPC class  - More char -> int8_t, wouldn't you know it?
    git-svn-id: https://dev.chiasoft.net:8443/svn/svn_vana/trunk@1315 23f3cdc0-e6d1-42be-a0c1-51f4125517bb

--- src/ChannelServer/NPCs.cpp
+++ src/ChannelServer/NPCs.cpp
@@ -178,7 +178,7 @@ bool NPC::run() {
 	return ret;
 }
 
-PacketCreator & NPC::npcPacket(char type) {
+PacketCreator & NPC::npcPacket(int8_t type) {
 	PacketCreator packet;
 	packet.addShort(SEND_NPC_TALK);
 	packet.addByte(4);
@@ -251,7 +251,7 @@ void NPC::sendGetNumber(int32_t def, int32_t min, int32_t max) {
 	player->getSession()->send(packet);
 }
 
-void NPC::sendStyle(int32_t styles[], char size) {
+void NPC::sendStyle(int32_t styles[], int8_t size) {
 	PacketCreator packet = npcPacket(NPCDialogs::style);
 	packet.addByte(size);
 	for (int32_t i = 0; i < size; i++)
