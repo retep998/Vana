f924a80b1ff9207c51b11fe291371906d58973bc
LazyBui <LazyBui@23f3cdc0-e6d1-42be-a0c1-51f4125517bb>
2008-09-13 21:47:46 +0000

    - Fixed compiler errors
    git-svn-id: https://dev.chiasoft.net:8443/svn/svn_vana/trunk@1328 23f3cdc0-e6d1-42be-a0c1-51f4125517bb

--- src/LoginServer/LoginServerAcceptPlayer.h
+++ src/LoginServer/LoginServerAcceptPlayer.h
@@ -19,6 +19,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #define LOGINSERVERACCEPTPLAYER_H
 
 #include "ServerPlayer.h"
+#include "Types.h"
 
 class ReadPacket;
 
