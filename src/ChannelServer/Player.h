9e7518dab6c1d506eb777b6abdc51d47516f2794
LazyBui <LazyBui@09c1253f-3d09-8e40-9719-4320e56601b5>
2008-09-13 19:15:04 +0000

    - Updated chars in Player  - Changed the layout of the Player class in the code
    git-svn-id: svn://70.113.44.214/VanaMirror/trunk@1309 09c1253f-3d09-8e40-9719-4320e56601b5

--- src/ChannelServer/Player.h
+++ src/ChannelServer/Player.h
@@ -50,77 +50,80 @@ public:
 	~Player();
 
 	void realHandleRequest(ReadPacket *packet);
-	int32_t getId() const { return this->id; }
-	int32_t getUserId() const { return this->userid; }
-	char getWorldId() const { return this->world_id; }
-	string getName() const { return this->name; }
-	char getGender() const { return this->gender; }
-	void setSkin(char id);
-	char getSkin() const { return this->skin; }
-	void setEyes(int32_t id);
-	int32_t getEyes() const { return this->eyes; }
-	void setHair(int32_t id);
-	int32_t getHair() const { return this->hair; }
-	void setLevel(unsigned char level);
-	unsigned char getLevel() const { return this->level; }
-	void setJob(int16_t job);
-	int16_t getJob() const { return this->job; }
-	void setStr(int16_t str);
-	int16_t getStr() const { return this->str; }
-	void setDex(int16_t dex);
-	int16_t getDex() const { return this->dex; }
-	void setInt(int16_t intt);
-	int16_t getInt() const { return this->intt; }
-	void setLuk(int16_t luk);
-	int16_t getLuk() const { return this->luk; }
-	void setHP(uint16_t hp, bool is = true);
-	uint16_t getHP() { return this->hp; }
-	void setMP(uint16_t mp, bool is = false);
-	uint16_t getMP() { return this->mp; }
-	void setMHP(uint16_t mhp);
-	uint16_t getMHP() { return this->mhp; }
-	void setRMHP(uint16_t rmhp);
-	uint16_t getRMHP() { return this->rmhp; }
-	void setMMP(uint16_t mmp);
-	uint16_t getMMP() { return this->mmp; }
-	void setRMMP(uint16_t rmmp);
-	uint16_t getRMMP() { return this->rmmp; }
-	void setHPMPAp(uint16_t ap) { hpmp_ap = ap; }
-	uint16_t getHPMPAp() { return hpmp_ap; }
+
+	void setSaveOnDC(bool save) { save_on_dc = save; }
+	void setTrading(int8_t newstate) { tradestate = newstate; }
+	void setSkin(int8_t id);
+	void setMappos(int8_t pos) { this->mappos = pos; }
+	void setLevel(uint8_t level);
 	void setAp(int16_t ap);
-	int16_t getAp() const { return this->ap; }
 	void setSp(int16_t sp);
-	int16_t getSp() const { return this->sp; }
 	void setFame(int16_t fame);
-	int16_t getFame() const { return this->fame; }
+	void setJob(int16_t job);
+	void setStr(int16_t str);
+	void setDex(int16_t dex);
+	void setInt(int16_t intt);
+	void setLuk(int16_t luk);
+	void setHP(uint16_t hp, bool is = true);
+	void setMP(uint16_t mp, bool is = false);
+	void setMHP(uint16_t mhp);
+	void setRMHP(uint16_t rmhp);
+	void setMMP(uint16_t mmp);
+	void setRMMP(uint16_t rmmp);
+	void setHPMPAp(uint16_t ap) { hpmp_ap = ap; }
+	void setEyes(int32_t id);
+	void setHair(int32_t id);
 	void setExp(int32_t exp);
-	int32_t getExp() const { return this->exp; }
 	void setMap(int32_t map) { this->map = map; }
-	int32_t getMap() const { return this->map; }
-	void setMappos(char pos) { this->mappos = pos; }
-	char getMappos() const { return this->mappos; }
+	void setTradeSendID(int32_t id) { this->tradesendid = id; }
+	void setTradeRecvID(int32_t id) { this->traderecvid = id; }
 	void setShop(int32_t shopid) { shop = shopid; }
-	int32_t getShop() const { return shop; }
 	void setNPC(NPC *npc) { this->npc = npc; }
 	void setChair(int32_t chair) { this->chair = chair; }
-	int32_t getChair() const { return chair; }
 	void setItemEffect(int32_t effect) { this->itemEffect = effect; }
+	void setSkill(SkillMapEnterActiveInfo skill) { this->skill = skill; }
+	void setSpecialSkill(SpecialSkillInfo info) { this->info = info; }
+
+	int8_t getWorldId() const { return this->world_id; }
+	int8_t getGender() const { return this->gender; }
+	int8_t getSkin() const { return this->skin; }
+	int8_t isTrading() const { return tradestate; }
+	int8_t getMappos() const { return this->mappos; }
+	uint8_t getLevel() const { return this->level; }
+	int16_t getJob() const { return this->job; }
+	int16_t getStr() const { return this->str; }
+	int16_t getDex() const { return this->dex; }
+	int16_t getInt() const { return this->intt; }
+	int16_t getLuk() const { return this->luk; }
+	int16_t getAp() const { return this->ap; }
+	int16_t getSp() const { return this->sp; }
+	int16_t getFame() const { return this->fame; }
+	uint16_t getHP() const { return this->hp; }
+	uint16_t getMP() const { return this->mp; }
+	uint16_t getMHP() const { return this->mhp; }
+	uint16_t getRMHP() const { return this->rmhp; }
+	uint16_t getMMP() const { return this->mmp; }
+	uint16_t getRMMP() const { return this->rmmp; }
+	uint16_t getHPMPAp() const { return hpmp_ap; }
+	int32_t getId() const { return this->id; }
+	int32_t getUserId() const { return this->userid; }
+	int32_t getEyes() const { return this->eyes; }
+	int32_t getHair() const { return this->hair; }
+	int32_t getExp() const { return this->exp; }
+	int32_t getMap() const { return this->map; }
+	int32_t getShop() const { return shop; }
+	int32_t getChair() const { return chair; }
 	int32_t getItemEffect() const { return this->itemEffect; }
+	int32_t getGMLevel() const { return gm; }
+	int32_t getSpecialSkill() const { return this->info.skillid; }
+	int32_t getTradeSendID() const { return tradesendid; }
+	int32_t getTradeRecvID() const { return traderecvid; }
+	string getName() const { return this->name; }
 	NPC * getNPC() const { return npc; }
 	bool isGM() const { return gm > 0; }
-	int32_t getGMLevel() const { return gm; }
-	void setSkill(SkillMapEnterActiveInfo skill) { this->skill = skill; }
-	void setSaveOnDC(bool save) { save_on_dc = save; }
-	SkillMapEnterActiveInfo getSkill() { return skill; }
-	void setSpecialSkill(SpecialSkillInfo info) { this->info = info; }
-	int32_t getSpecialSkill() const { return this->info.skillid; }
-	SpecialSkillInfo getSpecialSkillInfo() { return this->info; }
-	void setTrading(char newstate) { tradestate = newstate; }
-	char isTrading() const { return tradestate; }
-	void setTradeSendID(int32_t id) { this->tradesendid = id; }
-	int32_t getTradeSendID() const { return tradesendid; }
-	void setTradeRecvID(int32_t id) { this->traderecvid = id; }
-	int32_t getTradeRecvID() const { return traderecvid; }
+	SkillMapEnterActiveInfo getSkill() const { return skill; }
+	SpecialSkillInfo getSpecialSkillInfo() const { return this->info; }
+
 	bool hasGMEquip();
 
 	BuddyList * getBuddyList() const { return buddyList.get(); }
@@ -136,7 +139,7 @@ public:
 	string getVariable(const string &name);
 
 	bool addWarning();
-	void changeChannel(char channel);
+	void changeChannel(int8_t channel);
 	void saveStats();
 	void saveVariables();
 	void saveAll();
@@ -147,22 +150,20 @@ private:
 	void changeKey(ReadPacket *packet);
 	void changeSkillMacros(ReadPacket *packet);
 
-	int32_t id;
-	int32_t userid;
-	char world_id;
-	bool isconnect;
-	string name;
-	char gender;
-	char skin;
-	int32_t eyes;
-	int32_t hair;
-	unsigned char level;
+	int8_t world_id;
+	int8_t mappos;
+	int8_t tradestate;
+	int8_t gender;
+	int8_t skin;
+	uint8_t level;
 	int16_t job;
 	int16_t fame;
 	int16_t str;
 	int16_t dex;
 	int16_t intt;
 	int16_t luk;
+	int16_t ap;
+	int16_t sp;
 	uint16_t hp;
 	uint16_t mhp;
 	uint16_t rmhp;
@@ -170,22 +171,24 @@ private:
 	uint16_t mmp;
 	uint16_t rmmp;
 	uint16_t hpmp_ap;
-	int16_t ap;
-	int16_t sp;
+	int32_t id;
+	int32_t userid;
+	int32_t eyes;
+	int32_t hair;
 	int32_t exp;
 	int32_t map;
-	char mappos;
 	int32_t shop;
 	int32_t itemEffect;
 	int32_t chair;
 	int32_t gm;
-	char tradestate;
 	int32_t tradesendid;
 	int32_t traderecvid;
+	bool save_on_dc;
+	bool isconnect;
+	string name;
+	unordered_map<string, string> variables;
 	NPC *npc;
 	vector<int32_t> warnings;
-	bool save_on_dc;
-	unordered_map<string, string> variables;
 	SkillMapEnterActiveInfo skill;
 	SpecialSkillInfo info; // Hurricane/Pierce
 
