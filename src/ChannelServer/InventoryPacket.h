968bfb5d0507348290fa2b2bfdd5493ee79cfc74
LazyBui <LazyBui@09c1253f-3d09-8e40-9719-4320e56601b5>
2008-09-22 11:51:38 +0000

    - Fixed Legendary Spirit from freezing upon using a scroll you can't
    git-svn-id: svn://70.113.44.214/VanaMirror/trunk@1397 09c1253f-3d09-8e40-9719-4320e56601b5

--- src/ChannelServer/InventoryPacket.h
+++ src/ChannelServer/InventoryPacket.h
@@ -40,7 +40,7 @@ namespace InventoryPacket {
 	void useItem(Player *player, int32_t itemid, int32_t time, uint8_t types[8], const vector<int16_t> &vals, bool morph); // Use buff item
 	void endItem(Player *player, uint8_t types[8], bool morph);
 	void useSkillbook(Player *player, int32_t skillid, int32_t newMaxLevel, bool use, bool succeed);
-	void useScroll(Player *player, bool succeed, bool destroy, bool legendary_spirit);
+	void useScroll(Player *player, int8_t succeed, bool destroy, bool legendary_spirit);
 	void showMegaphone(Player *player, const string &msg);
 	void showSuperMegaphone(Player *player, const string & msg, uint8_t whisper = 0);
 	void showMessenger(Player *player, const string &msg, const string &msg2, const string &msg3, const string &msg4, unsigned char *displayInfo, int32_t displayInfo_size, int32_t itemid);
