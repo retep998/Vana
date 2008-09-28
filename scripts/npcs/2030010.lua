95a2d01e8b41bf588f873ad5945b38d740a9d2f3
LazyBui <LazyBui@23f3cdc0-e6d1-42be-a0c1-51f4125517bb>
2008-09-28 15:16:31 +0000

    - Updated Amon to include his jump quest counterparts
    git-svn-id: https://dev.chiasoft.net:8443/svn/svn_vana/trunk@1432 23f3cdc0-e6d1-42be-a0c1-51f4125517bb

--- scripts/npcs/2030010.lua
+++ scripts/npcs/2030010.lua
@@ -15,19 +15,32 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 --]]
--- Amon; Zakum's Altar bouncer
-if state == 0 then
-	addText("Are you sure you want to leave this place? You are entitled to enter the Zakum Altar up to twice a day, and by leaving right now, you may only re-enter this shrine once more for the rest of the day.");
-	sendYesNo();
-elseif state == 1 then
-	if getSelected() == 1 then
-		if getNumPlayers(280030000) == 1 then
-			setReactorsState(280030000, 2111001, 0); -- Zakum's altar
-			setReactorsState(211042300, 2118002, 0); -- Zakum's door
-			clearDrops();
-			clearMobs();
+-- Amon; Zakum's Altar bouncer, also present in the Zakum quests
+
+if getMap() == 280030000 then -- Zakum's Altar
+	if state == 0 then
+		addText("Are you sure you want to leave this place? You are entitled to enter the Zakum Altar up to twice a day, and by leaving right now, you may only re-enter this shrine once more for the rest of the day.");
+		sendYesNo();
+	elseif state == 1 then
+		if getSelected() == 1 then
+			if getNumPlayers(280030000) == 1 then
+				setReactorsState(280030000, 2111001, 0); -- Zakum's altar
+				setReactorsState(211042300, 2118002, 0); -- Zakum's door
+				clearDrops();
+				clearMobs();
+			end
+			setMap(211042300); -- Door to Zakum
+			end
+		endNPC();
+	end
+else -- Maps 280020000 and 280020001, Zakum's jump quest
+	if state == 0 then
+		addText("Are you sure you want to quit and leave this place? Next time you come back in, you'll have to start all over again.");
+		sendYesNo();
+	elseif state == 1 then
+		if getSelected() == 1 then
+			setMap(211042300); -- Door to Zakum
 		end
-		setMap(211042300); -- Door to Zakum
-		end
-	endNPC();
+		endNPC();
+	end
 end
