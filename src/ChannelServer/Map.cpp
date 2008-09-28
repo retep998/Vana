86d7a18910619be89e92f4c62bed1bcbfeaf7843
LazyBui <LazyBui@09c1253f-3d09-8e40-9719-4320e56601b5>
2008-09-28 22:43:50 +0000

    - Fixed a small bug in findFloor that would cause some drops to float
    git-svn-id: svn://70.113.44.214/VanaMirror/trunk@1440 09c1253f-3d09-8e40-9719-4320e56601b5

--- src/ChannelServer/Map.cpp
+++ src/ChannelServer/Map.cpp
@@ -104,7 +104,7 @@ Pos Map::findFloor(Pos pos) {
 	int16_t maxy = pos.y;
 	bool firstcheck = true;
 	for (size_t i = 0; i < footholds.size(); i++) {
-		if ((x >= footholds[i].pos1.x && x <= footholds[i].pos2.x) || (x >= footholds[i].pos2.x && x <= footholds[i].pos1.x)) {
+		if ((x > footholds[i].pos1.x && x < footholds[i].pos2.x) || (x > footholds[i].pos2.x && x < footholds[i].pos1.x)) {
 			int16_t cmax = (int16_t)((float)(footholds[i].pos1.y - footholds[i].pos2.y) / (footholds[i].pos1.x - footholds[i].pos2.x) * (x - footholds[i].pos1.x) + footholds[i].pos1.y);
 			if ((cmax <= maxy || (maxy == pos.y && firstcheck)) && cmax >= y) {
 				maxy = cmax;
