c153fd36dbc20316550506d72cd863f5636bd654
pawitp <pawitp@23f3cdc0-e6d1-42be-a0c1-51f4125517bb>
2008-10-15 08:35:08 +0000

    Fixed inclusion guard in Database.h
    git-svn-id: https://dev.chiasoft.net:8443/svn/svn_vana/trunk@1524 23f3cdc0-e6d1-42be-a0c1-51f4125517bb

--- src/Common/Database.h
+++ src/Common/Database.h
@@ -15,8 +15,8 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
-#ifndef MYSQL_H
-#define MYSQL_H
+#ifndef DATABASE_H
+#define DATABASE_H
 
 #ifdef WIN32
 # include <winsock2.h> // Prevent conflict with boost::asio
