5b1d0abc356a6a52fa5549981f5693189f9a4818
pawitp <pawitp@09c1253f-3d09-8e40-9719-4320e56601b5>
2008-10-19 14:49:48 +0000

    Replaced most of StringUtilities with boost string algorithms
    git-svn-id: svn://70.113.44.214/VanaMirror/trunk@1541 09c1253f-3d09-8e40-9719-4320e56601b5

--- src/ChannelServer/Players.cpp
+++ src/ChannelServer/Players.cpp
@@ -19,18 +19,18 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include "MapleSession.h"
 #include "PacketCreator.h"
 #include "Player.h"
-#include "StringUtilities.h"
+#include <boost/algorithm/string.hpp>
 
 Players * Players::singleton = 0;
 
 void Players::addPlayer(Player *player) {
 	m_players[player->getId()] = player;
-	m_players_names[StringUtilities::toUpper(player->getName())] = player; // Store in upper case for easy non-case-sensitive search
+	m_players_names[boost::to_upper_copy(player->getName())] = player; // Store in upper case for easy non-case-sensitive search
 }
 
 void Players::removePlayer(Player *player) {
 	m_players.erase(player->getId());
-	m_players_names.erase(StringUtilities::toUpper(player->getName()));
+	m_players_names.erase(boost::to_upper_copy(player->getName()));
 }
 
 Player * Players::getPlayer(int32_t id) {
@@ -38,7 +38,7 @@ Player * Players::getPlayer(int32_t id) {
 }
 
 Player * Players::getPlayer(const string &name) {
-	string upCaseName = StringUtilities::toUpper(name);
+	string upCaseName = boost::to_upper_copy(name);
 	return (m_players_names.find(upCaseName) == m_players_names.end()) ? 0 : m_players_names[upCaseName];
 }
 
