d04de80dc329452c5cb41dc041e70498b18cd7f1
LazyBui <LazyBui@23f3cdc0-e6d1-42be-a0c1-51f4125517bb>
2008-12-03 02:55:55 +0000

    - Cleaned up MapleEncryption  - Improved performance by no less than 12 instructions per packet (3 useless multiplications performed 4 times for no reason)  - Improved memory performance of the encryption routines  - Changed MapleEncryption from a class to a namespace, as it was not used like a class  - Cleaned up Decoder.cpp and made a couple changes
    git-svn-id: https://dev.chiasoft.net:8443/svn/svn_vana/trunk@1651 23f3cdc0-e6d1-42be-a0c1-51f4125517bb

--- src/Common/Decoder.cpp
+++ src/Common/Decoder.cpp
@@ -24,16 +24,17 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 void Decoder::encrypt(unsigned char *buffer, int32_t size) {
 	MapleEncryption::mapleEncrypt(buffer, size);
-	int32_t pos=0,first=1;
+	int32_t pos = 0;
+	uint8_t first = 1;
 	while (size > pos) {
-		if (size>pos+1460-first*4) {
-			decryptofb(buffer+pos, Decoder::ivSend, 1460 - first*4);
+		if (size > (pos + 1460 - first * 4)) {
+			decryptofb(buffer + pos, Decoder::ivSend, 1460 - first * 4);
 		}
 		else
-			decryptofb(buffer+pos, Decoder::ivSend, size-pos);
-		pos+=1460-first*4;
-		if (first)
-			first=0;
+			decryptofb(buffer + pos, Decoder::ivSend, size - pos);
+		pos += 1460 - first * 4;
+		if (first == 1)
+			first = 0;
 	}
 } 
  
@@ -48,25 +49,25 @@ void Decoder::decrypt(unsigned char *buffer, int32_t size) {
 }
 
 void Decoder::createHeader (unsigned char *header, int16_t size) {
-	int16_t a = ivSend[3]*0x100 + ivSend[2];
-	a = a ^ -(MAPLE_VERSION+1);
+	int16_t a = ivSend[3] * 0x100 + ivSend[2];
+	a = a ^ -(MAPLE_VERSION + 1);
 	int16_t b = a ^ size; 
-	header[0] = a%0x100;
-	header[1] = (a-header[0])/0x100;
-	header[2] = b%0x100;
-	header[3] = (b-header[2])/0x100;
+	header[0] = a % 0x100;
+	header[1] = (a - header[0]) / 0x100;
+	header[2] = b % 0x100;
+	header[3] = (b - header[2]) / 0x100;
 }
 
 PacketCreator Decoder::getConnectPacket(string unknown) {
-	(*(int32_t *)ivRecv) = Randomizer::Instance()->randInt();
-	(*(int32_t *)ivSend) = Randomizer::Instance()->randInt();
+	(*(int32_t*)ivRecv) = Randomizer::Instance()->randInt();
+	(*(int32_t*)ivSend) = Randomizer::Instance()->randInt();
 
 	PacketCreator packet;
 	packet.addShort(0); // Packet len, this will be added later in the packet
 	packet.addShort(MAPLE_VERSION);
 	packet.addString(unknown); // Unknown, the official login server sends a "0", the channel server sends nothing
-	packet.addInt(*(int32_t *) ivRecv);
-	packet.addInt(*(int32_t *) ivSend);
+	packet.addInt(*(int32_t*) ivRecv);
+	packet.addInt(*(int32_t*) ivSend);
 	packet.addByte(MAPLE_VERSION_UNK);
 
 	packet.setShort(packet.getSize() - 2, 0); // -2 as the size does not include the size of the size header
