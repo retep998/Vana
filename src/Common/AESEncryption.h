0988e38cbcd6dce463355712cd2547e00856f7fb
doyos <doyos@09c1253f-3d09-8e40-9719-4320e56601b5>
2008-12-04 16:18:02 +0000

    Cleaned up AESEncryption, AKA removed the arseload of fail.
    git-svn-id: svn://70.113.44.214/VanaMirror/trunk@1652 09c1253f-3d09-8e40-9719-4320e56601b5

--- src/Common/AESEncryption.h
+++ src/Common/AESEncryption.h
@@ -18,6 +18,42 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #ifndef AES_ENCRYPT_H
 #define AES_ENCRYPT_H
 
-void decryptofb(unsigned char *buffer, unsigned char *vec, int bsize);
+enum keySize {
+	SIZE_16 = 16,
+	SIZE_24 = 24,
+	SIZE_32 = 32
+};
+
+class AESEncryption {
+public:
+	static AESEncryption * Instance() {
+		if (singleton == 0)
+			singleton = new AESEncryption;
+		return singleton;
+	}
+	void decryptofb(uint8_t *buffer, uint8_t *vec, int32_t bsize);
+
+private:
+	AESEncryption() {}
+	static AESEncryption *singleton;
+	static const uint8_t sbox[256];
+	static const uint8_t Rcon[255];
+	static const uint8_t aes_key[32];
+
+	void rotate(uint8_t *word);
+	void core(uint8_t *word, int32_t iteration);
+	void expandKey(uint8_t *expandedKey, enum keySize size, size_t expandedKeySize);
+	void subBytes(uint8_t *state);
+	void shiftRow(uint8_t *state, uint8_t nbr);
+	void shiftRows(uint8_t *state);
+	void addRoundKey(uint8_t *state, uint8_t *roundKey);
+	uint8_t galois_multiplication(uint8_t a, uint8_t b);
+	void mixColumn(uint8_t *column);
+	void mixColumns(uint8_t *state);
+	void aes_round(uint8_t *state, uint8_t *roundKey);
+	void createRoundKey(uint8_t *expandedKey, uint8_t *roundKey);
+	void aes_main(uint8_t *state, uint8_t *expandedKey, int32_t nbrRounds);
+	int8_t aes_encrypt(uint8_t *input, uint8_t *output, enum keySize size);
+};
 
 #endif
