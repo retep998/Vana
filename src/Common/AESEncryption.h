e1e93c1aa20427881b96f93899e73ff94074d431
doyos <doyos@23f3cdc0-e6d1-42be-a0c1-51f4125517bb>
2008-12-05 06:29:48 +0000

    Added include of Types.h to AESEncryption due to VC++ not supporting unit8_t and such by standard.
    git-svn-id: https://dev.chiasoft.net:8443/svn/svn_vana/trunk@1653 23f3cdc0-e6d1-42be-a0c1-51f4125517bb

--- src/Common/AESEncryption.h
+++ src/Common/AESEncryption.h
@@ -18,6 +18,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #ifndef AES_ENCRYPT_H
 #define AES_ENCRYPT_H
 
+#include "Types.h"
+
 enum keySize {
 	SIZE_16 = 16,
 	SIZE_24 = 24,
