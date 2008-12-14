93d2d40f15f26dc38e1387b32facc60e2078efb2
LazyBui <LazyBui@09c1253f-3d09-8e40-9719-4320e56601b5>
2008-12-14 14:16:23 +0000

    - Fixed Dash entirely (it has a unique packet, bastards)  - Separated unique packets (mounts, Dash) out of useSkill  - "Thanks" to DeanSan for bugging me about it
    git-svn-id: svn://70.113.44.214/VanaMirror/trunk@1683 09c1253f-3d09-8e40-9719-4320e56601b5

--- src/ChannelServer/SkillsPacket.h
+++ src/ChannelServer/SkillsPacket.h
@@ -28,7 +28,9 @@ struct SpecialSkillInfo;
 namespace SkillsPacket {
 	void addSkill(Player *player, int32_t skillid, PlayerSkillInfo skillinfo);
 	void showSkill(Player *player, int32_t skillid, uint8_t level, bool party = false, bool self = false);
-	void useSkill(Player *player, int32_t skillid, int32_t time, SkillActiveInfo pskill, SkillActiveInfo mskill, int16_t addedinfo, int32_t mountid = 0);
+	void useDash(Player *player, int32_t time, SkillActiveInfo pskill, SkillActiveInfo mskill);
+	void useSkill(Player *player, int32_t skillid, int32_t time, SkillActiveInfo pskill, SkillActiveInfo mskill, int16_t addedinfo);
+	void useMount(Player *player, int32_t skillid, int32_t time, SkillActiveInfo pskill, SkillActiveInfo mskill, int16_t addedinfo, int32_t mountid);
 	void endSkill(Player *player, SkillActiveInfo pskill, SkillActiveInfo mskill);
 	void healHP(Player *player, int16_t hp);
 	void showSkillEffect(Player *player, int32_t skillid, uint8_t level = 0);
