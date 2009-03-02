ede6862c206aa139c9a37d68834384b3cbfe0306
pawitp <pawitp@09c1253f-3d09-8e40-9719-4320e56601b5>
2009-03-02 14:50:05 +0000

    - Added missing #includes
    git-svn-id: svn://70.113.44.214/VanaMirror/trunk@1777 09c1253f-3d09-8e40-9719-4320e56601b5

--- src/ChannelServer/PlayerActiveBuffs.h
+++ src/ChannelServer/PlayerActiveBuffs.h
@@ -21,6 +21,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include "Buffs.h"
 #include "Types.h"
 #include <list>
+#include <boost/tr1/memory.hpp>
 #include <boost/tr1/unordered_map.hpp>
 #include <vector>
 
--- src/WorldServer/Channels.h
+++ src/WorldServer/Channels.h
@@ -19,6 +19,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #define CHANNELS_H
 
 #include "Types.h"
+#include <boost/tr1/memory.hpp>
 #include <boost/tr1/unordered_map.hpp>
 #include <string>
 
