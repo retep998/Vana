86cea8f95df157b13c2d3f3cc5e1171a39197121
pawitp <pawitp@09c1253f-3d09-8e40-9719-4320e56601b5>
2009-03-03 01:37:03 +0000

    Added missing includes needed to compile on gcc (linux) according to Jonathan
    git-svn-id: svn://70.113.44.214/VanaMirror/trunk@1781 09c1253f-3d09-8e40-9719-4320e56601b5

--- src/Common/MiscUtilities.h
+++ src/Common/MiscUtilities.h
@@ -18,6 +18,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #ifndef MISCUTILITIES
 #define MISCUTILITIES
 
+#include <cstdlib>
 #include <string>
 
 using std::string;
