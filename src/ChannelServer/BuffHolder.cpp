50b707ee42d4ee10cd72ba1daf7bf1a9dae0036f
LazyBui <LazyBui@09c1253f-3d09-8e40-9719-4320e56601b5>
2009-04-10 14:50:21 +0000

    - Another missed #include
    git-svn-id: svn://70.113.44.214/VanaMirror/trunk@1915 09c1253f-3d09-8e40-9719-4320e56601b5

--- src/ChannelServer/BuffHolder.cpp
+++ src/ChannelServer/BuffHolder.cpp
@@ -16,6 +16,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 #include "BuffHolder.h"
+#include "ChannelServer.h"
 #include "PacketReader.h"
 #include "PlayerActiveBuffs.h"
 #include "WorldServerConnectPacket.h"
