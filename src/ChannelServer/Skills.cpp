f5761116e0dd14fd0725500aecd3e4d607ac7077
LazyBui <LazyBui@09c1253f-3d09-8e40-9719-4320e56601b5>
2009-04-12 02:50:23 +0000

    - And again, removing my fun little header file from the commit  - Credit to Tainted for pointing it out
    git-svn-id: svn://70.113.44.214/VanaMirror/trunk@1935 09c1253f-3d09-8e40-9719-4320e56601b5

--- src/ChannelServer/Skills.cpp
+++ src/ChannelServer/Skills.cpp
@@ -33,8 +33,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include "Timer/Timer.h"
 #include <functional>
 
-#include "displaypacket.h"
-
 using std::tr1::bind;
 
 unordered_map<int32_t, SkillsLevelInfo> Skills::skills;
