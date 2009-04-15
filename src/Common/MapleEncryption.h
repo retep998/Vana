dbb95689f50f01f83cc3a1b42af56c5733178e95
pawitp <pawitp@09c1253f-3d09-8e40-9719-4320e56601b5>
2009-04-15 02:49:04 +0000

    Using Crypto++ for AES-OFB Encryption
    git-svn-id: svn://70.113.44.214/VanaMirror/trunk@1986 09c1253f-3d09-8e40-9719-4320e56601b5

--- src/Common/MapleEncryption.h
+++ src/Common/MapleEncryption.h
@@ -19,13 +19,23 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #define MAPLEENC_H
 
 #include "Types.h"
+#include <cstring>
 
 namespace MapleEncryption {
-	void nextIV(unsigned char *vector);
+	void nextIv(unsigned char *vector);
 	void mapleDecrypt(unsigned char *buf, int32_t size);
 	void mapleEncrypt(unsigned char *buf, int32_t size);
+	void setIv(unsigned char *dest, unsigned char *source);
 	uint8_t ror(uint8_t val, int32_t num);
 	uint8_t rol(uint8_t val, int32_t num);
 };
+
+inline
+void MapleEncryption::setIv(unsigned char *dest, unsigned char *source) {
+	// The 16 bit IV is the 4 bit IV repeated 4 times
+	for (uint8_t i = 0; i < 4; i++) {
+		memcpy(dest + i*4, source, 4);
+	}
+}
 
 #endif
