retep998/Vana

commit 33e140078bdf6d312f7e6dc8dda795e83d40efb9

- Made portal in Maps::getMap an optional parameter  - Added support for "/m" GM Command
git-svn-id: svn://70.113.44.214/VanaMirror/trunk@2076 09c1253f-3d09-8e40-9719-4320e56601b5

src/ChannelServer/Maps.cpp
@@ -73,25 +73,32 @@ void Maps::usePortal(Player *player, PortalInfo *portal) {
 
 void Maps::usePortal(Player *player, PacketReader &packet) {
 	packet.skipBytes(1);
-	if (packet.get<int32_t>() == 0) { // Dead
-		if (player->getHp() == 0) {
-			player->acceptDeath();
+
+	int32_t opcode = packet.get<int32_t>();
+	switch(opcode) {
+		case 0: // Dead
+			if (player->getHp() == 0) { // else, hacking
+				player->acceptDeath();
+			}
+		case -1: { // The fall through is intentional
+			string portalname = packet.getString();
+
+			Map *tomap = getMap(player->getMap());
+			if (tomap == 0)
+				return;
+			PortalInfo *portal = tomap->getPortal(portalname);
+			if (portal == 0) // Exit the function if portal is not found
+				return;
+
+			usePortal(player, portal);
+			break;
 		}
-		else {
-			// hacking
+		default: { // GM Map change (command "/m")
+			if (player->isGm() && getMap(opcode)) {
+				changeMap(player, opcode);
+			}
 		}
-		return;
 	}
-	string portalname = packet.getString();
-
-	Map *tomap = getMap(player->getMap());
-	if (tomap == 0)
-		return;
-	PortalInfo *portal = tomap->getPortal(portalname);
-	if (portal == 0) // Exit the function if portal is not found
-		return;
-
-	usePortal(player, portal);
 }
 
 void Maps::useScriptedPortal(Player *player, PacketReader &packet) {

src/ChannelServer/ChatHandler.cpp
@@ -56,7 +56,7 @@ struct MeFunctor {
 struct WarpFunctor {
 	void operator() (Player *warpee) {
 		if (warpee->getMap() != mapid) {
-			Maps::changeMap(warpee, mapid, 0);
+			Maps::changeMap(warpee, mapid);
 		}
 	}
 	int32_t mapid;
@@ -273,7 +273,7 @@ void ChatHandler::handleChat(Player *player, PacketReader &packet) {
 							int32_t mapid = mapstring.length() > 0 ? atoi(mapstring.c_str()) : player->getMap();
 
 							if (Maps::getMap(mapid)) {
-								Maps::changeMap(warpee, mapid, 0);
+								Maps::changeMap(warpee, mapid);
 							}
 						}
 					}
@@ -441,7 +441,7 @@ void ChatHandler::handleChat(Player *player, PacketReader &packet) {
 							if (strlen(endptr) != 0) mapid = -1;
 						}
 						if (Maps::getMap(mapid))
-							Maps::changeMap(player, mapid, 0);
+							Maps::changeMap(player, mapid);
 						else
 							PlayerPacket::showMessage(player, "Invalid Map ID", 6);
 					}
@@ -697,7 +697,7 @@ void ChatHandler::handleChat(Player *player, PacketReader &packet) {
 				case CmdWarpTo:
 					Player *warptoee;
 					if (warptoee = Players::Instance()->getPlayer(args)) {
-						Maps::changeMap(player, warptoee->getMap(), 0);
+						Maps::changeMap(player, warptoee->getMap());
 					}
 					break;
 				case CmdZakum:

src/ChannelServer/Instance.cpp
@@ -153,7 +153,7 @@ void Instance::moveAllPlayers(int32_t mapid) {
 	if (!Maps::getMap(mapid))
 		return;
 	for (unordered_map<int32_t, Player *>::iterator iter = m_players.begin(); iter != m_players.end(); iter++) {
-		Maps::changeMap(iter->second, mapid, 0);
+		Maps::changeMap(iter->second, mapid);
 	}
 }
 

src/ChannelServer/Inventory.cpp
@@ -585,9 +585,9 @@ void Inventory::useReturnScroll(Player *player, PacketReader &packet) {
 	takeItemSlot(player, 2, slot, 1);
 	int32_t map = ItemDataProvider::Instance()->getItemInfo(itemid).cons.moveTo;
 	if (map == 999999999)
-		Maps::changeMap(player, Maps::getMap(player->getMap())->getInfo()->rm, 0);
+		Maps::changeMap(player, Maps::getMap(player->getMap())->getInfo()->rm);
 	else
-		Maps::changeMap(player, map, 0);
+		Maps::changeMap(player, map);
 }
 
 void Inventory::useScroll(Player *player, PacketReader &packet) {

src/ChannelServer/Maps.h
@@ -33,7 +33,7 @@ namespace Maps {
 	void usePortal(Player *player, PortalInfo *portal);
 	void usePortal(Player *player, PacketReader &packet);
 	void useScriptedPortal(Player *player, PacketReader &packet);
-	void changeMap(Player *player, int32_t mapid, PortalInfo *portal);
+	void changeMap(Player *player, int32_t mapid, PortalInfo *portal = 0);
 	void newMap(Player *player, int32_t mapid);
 };
 

src/ChannelServer/Player.cpp
@@ -650,7 +650,7 @@ void Player::acceptDeath() {
 	int32_t tomap = (Maps::getMap(map) ? Maps::getMap(map)->getInfo()->rm : map);
 	setHp(50, false);
 	getActiveBuffs()->removeBuff();
-	Maps::changeMap(this, tomap, 0);
+	Maps::changeMap(this, tomap);
 }
 
 bool Player::hasGmEquip() {

src/ChannelServer/PlayerHandler.cpp
@@ -259,7 +259,7 @@ void PlayerHandler::handleMoving(Player *player, PacketReader &packet) {
 			if (floor.y == playerpos.y) { // There are no footholds below the player
 				int8_t count = player->getFallCounter();
 				if (count > 3) {
-					Maps::changeMap(player, mapid, 0);
+					Maps::changeMap(player, mapid);
 					player->setFallCounter(0);
 				}
 				else {
@@ -268,7 +268,7 @@ void PlayerHandler::handleMoving(Player *player, PacketReader &packet) {
 			}
 		}
 		else if (playerpos.y > bottom) {
-			Maps::changeMap(player, mapid, 0);
+			Maps::changeMap(player, mapid);
 		}
 	}
 	else {

commit 2af053063ce5a56f13ed729cf490731ba908da50

Fixed a bug introduced in r2076
git-svn-id: svn://70.113.44.214/VanaMirror/trunk@2078 09c1253f-3d09-8e40-9719-4320e56601b5

src/ChannelServer/Maps.cpp
@@ -80,7 +80,8 @@ void Maps::usePortal(Player *player, PacketReader &packet) {
 			if (player->getHp() == 0) { // else, hacking
 				player->acceptDeath();
 			}
-		case -1: { // The fall through is intentional
+			break;
+		case -1: {
 			string portalname = packet.getString();
 
 			Map *tomap = getMap(player->getMap());