480b4d8e31119abe6e53632ecaef5869aaaafc84
pawitp <pawitp@23f3cdc0-e6d1-42be-a0c1-51f4125517bb>
2009-05-10 12:57:53 +0000

    Added advice{00..09}.lua portal scripts (not including adviceMap.lua)
    git-svn-id: https://dev.chiasoft.net:8443/svn/svn_vana/branches/new_version_support@2091 23f3cdc0-e6d1-42be-a0c1-51f4125517bb

--- scripts/portals/advice05.lua
+++ scripts/portals/advice05.lua
@@ -15,4 +15,4 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 --]]
-showInstructionBubble("Press #e#b[S]#k#n to view your character information.", 250, 5);
+showInstructionBubble("Press #e#b[Q]#k#n to view the Quest window.", 250, 5);
