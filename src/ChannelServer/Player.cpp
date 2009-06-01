e4fc8669dfd41dd1912e73c6a7ea3f53e6a1c65d
doyos <doyos@23f3cdc0-e6d1-42be-a0c1-51f4125517bb>
2009-06-01 13:06:31 +0000

    Best revision.
    git-svn-id: https://dev.chiasoft.net:8443/svn/svn_vana/trunk@2189 23f3cdc0-e6d1-42be-a0c1-51f4125517bb

--- src/ChannelServer/Player.cpp
+++ src/ChannelServer/Player.cpp
@@ -15,7 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
-#include "Player.h"
+#include "Player.h" // Emmy was here
 #include "BuddyListHandler.h"
 #include "BuddyListPacket.h"
 #include "ChannelServer.h"
