retep998/Vana

commit bd1c8e63d473d978b9e413cdab4d47103ad98771

Rudimentary mount tracking support. Note: The SQL file will delete all your players' current mounts. They won't be trackable without getting new ones. Simply remove the delete from query if you do not care.
Known bugs/features not implemented:
 - EXP increasing
 - Tiredness stuff
 - When you throw stars or use arrows, the mount information in the player info box will be screwed up

git-svn-id: svn://70.113.44.214/VanaMirror/trunk@2198 09c1253f-3d09-8e40-9719-4320e56601b5

sql/0060_mount_table.sql
@@ -1,0 +1,11 @@
+CREATE TABLE `mounts` (
+  `charid` int(11) NOT NULL,
+  `mountid` int(11) NOT NULL,
+  `exp` smallint(6) DEFAULT '0',
+  `level` int(3) unsigned NOT NULL DEFAULT '1',
+  `tiredness` int(3) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`charid`,`mountid`),
+  KEY `mountid` (`mountid`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+
+DELETE FROM `items` WHERE FLOOR(`items.itemid` / 10000) = 190;

ChannelServer.vcproj
@@ -514,6 +514,10 @@
 					>
 				</File>
 				<File
+					RelativePath=".\src\ChannelServer\PlayerMounts.cpp"
+					>
+				</File>
+				<File
 					RelativePath=".\src\ChannelServer\PlayerPacketHolder.cpp"
 					>
 				</File>
@@ -877,6 +881,10 @@
 				</File>
 				<File
 					RelativePath=".\src\ChannelServer\PlayerInventory.h"
+					>
+				</File>
+				<File
+					RelativePath=".\src\ChannelServer\PlayerMounts.h"
 					>
 				</File>
 				<File

src/ChannelServer/CMakeLists.txt
@@ -50,6 +50,7 @@ set(SOURCES
 	PlayerBuddyList.h
 	PlayerHandler.h
 	PlayerInventory.h
+	PlayerMounts.h
 	PlayerPacket.h
 	PlayerPacketHelper.h
 	PlayerPacketHolder.h
@@ -129,6 +130,7 @@ set(SOURCES
 	PlayerBuddyList.cpp
 	PlayerHandler.cpp
 	PlayerInventory.cpp
+	PlayerMounts.cpp
 	PlayerPacket.cpp
 	PlayerPacketHelper.cpp
 	PlayerPacketHolder.cpp

src/ChannelServer/GameConstants.h
@@ -51,6 +51,7 @@ enum ItemTypes {
 	WeaponClaw = 147,
 	WeaponKnuckle = 148,
 	WeaponGun = 149,
+	Mount = 190,
 	ItemArrow = 206,
 	ItemStar = 207,
 	ItemBullet = 233

src/ChannelServer/GameLogicUtilities.h
@@ -36,6 +36,7 @@ namespace GameLogicUtilities {
 	inline bool isShield(int32_t itemid) { return (getItemType(itemid) == ArmorShield); }
 	inline bool is2hWeapon(int32_t itemid) { return (getItemType(itemid) / 10 == 14); }
 	inline bool is1hWeapon(int32_t itemid) { return (getItemType(itemid) / 10 == 13); }
+	inline bool isMount(int32_t itemid) { return (getItemType(itemid) == Mount); }
 	inline bool isBeginnerSkill(int32_t skillid) { return ((skillid / 1000000) == (skillid < 10000000 ? 0 : 10)); }
 	inline bool isFourthJobSkill(int32_t skillid) { return ((skillid / 10000) % 10 == 2); }
 	inline bool isSummon(int32_t skillid) { return (skillid == Jobs::Sniper::Puppet || skillid == Jobs::Ranger::Puppet || skillid == Jobs::WindBreaker::Puppet || skillid == Jobs::Priest::SummonDragon || skillid == Jobs::Ranger::SilverHawk || skillid == Jobs::Sniper::GoldenEagle || skillid == Jobs::DarkKnight::Beholder || skillid == Jobs::FPArchMage::Elquines || skillid == Jobs::ILArchMage::Ifrit || skillid == Jobs::FlameWizard::Ifrit || skillid == Jobs::Bishop::Bahamut || skillid == Jobs::Bowmaster::Phoenix || skillid == Jobs::Marksman::Frostprey || skillid == Jobs::Outlaw::Octopus || skillid == Jobs::Outlaw::Gaviota || skillid == Jobs::SoulWarrior::Soul || skillid == Jobs::FlameWizard::Flame || skillid == Jobs::WindBreaker::Storm || skillid == Jobs::NightWalker::Darkness || skillid == Jobs::Striker::Lightning); }
@@ -46,8 +47,8 @@ namespace GameLogicUtilities {
 	inline bool isMobSkill(int32_t skillid) { return (skillid >= 100 && skillid <= 200); }
 	inline bool isCygnus(int16_t jobid) { return (jobid >= 1000); }
 	inline bool isBeginnerJob(int16_t jobid) { return (jobid == 0 || jobid == 1000); }
-	inline int8_t getMasteryDisplay(int8_t level) { return ((level + 1) / 2); }
 	inline int16_t getJobTrack(int16_t job, bool flattencygnus = false) { return (flattencygnus && isCygnus(job) ? ((job / 100) % 10) : (job / 100)); }
+	inline int8_t getMasteryDisplay(int8_t level) { return ((level + 1) / 2); }
 	inline int8_t getPartyMember1(int8_t totalmembers) { return (totalmembers >= 1 ? (0x40 >> totalmembers) : 0xFF); }
 	inline int8_t getPartyMember2(int8_t totalmembers) { return (totalmembers >= 2 ? (0x80 >> totalmembers) : 0xFF); }
 	inline int8_t getPartyMember3(int8_t totalmembers) { return (totalmembers >= 3 ? (0x100 >> totalmembers) : 0xFF); }

src/ChannelServer/Inventory.cpp
@@ -398,13 +398,17 @@ void Inventory::addNewItem(Player *player, int32_t itemid, int16_t amount) {
 	}
 
 	Item *item = 0;
-	if (GameLogicUtilities::isEquip(itemid))
+	if (GameLogicUtilities::isEquip(itemid)) {
 		item = new Item(itemid, false);
-	else
+		if (GameLogicUtilities::isMount(itemid))
+			player->getMounts()->addMount(itemid);
+	}
+	else {
 		item = new Item(itemid, thisamount);
-
-	if (addItem(player, item, GameLogicUtilities::isPet(itemid)) == 0 && amount > 0)
+	}
+	if (addItem(player, item, GameLogicUtilities::isPet(itemid)) == 0 && amount > 0) {
 		addNewItem(player, itemid, amount);
+	}
 }
 
 void Inventory::takeItem(Player *player, int32_t itemid, uint16_t howmany) {

src/ChannelServer/Player.cpp
@@ -230,6 +230,7 @@ void Player::playerConnect(PacketReader &packet) {
 	m_foothold = 0;
 
 	// Inventory
+	mounts.reset(new PlayerMounts(this));
 	pets.reset(new PlayerPets(this));
 	boost::array<uint8_t, Inventories::InventoryCount> maxslots;
 	maxslots[0] = static_cast<uint8_t>(res[0]["equip_slots"]);
@@ -648,6 +649,7 @@ void Player::saveStats() {
 void Player::saveAll(bool savecooldowns) {
 	saveStats();
 	getInventory()->save();
+	getMounts()->save();
 	getPets()->save();
 	getSkills()->save(savecooldowns);
 	getStorage()->save();

src/ChannelServer/Player.h
@@ -24,6 +24,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include "PlayerActiveBuffs.h"
 #include "PlayerBuddyList.h"
 #include "PlayerInventory.h"
+#include "PlayerMounts.h"
 #include "PlayerPets.h"
 #include "PlayerQuests.h"
 #include "PlayerSkills.h"
@@ -145,6 +146,7 @@ public:
 	PlayerActiveBuffs * getActiveBuffs() const { return activeBuffs.get(); }
 	PlayerBuddyList * getBuddyList() const { return buddyList.get(); }
 	PlayerInventory * getInventory() const { return inv.get(); }
+	PlayerMounts * getMounts() const { return mounts.get(); }
 	PlayerPets * getPets() const { return pets.get(); }
 	PlayerQuests * getQuests() const { return quests.get(); }
 	PlayerSkills * getSkills() const { return skills.get(); }
@@ -219,6 +221,7 @@ private:
 	scoped_ptr<PlayerActiveBuffs> activeBuffs;
 	scoped_ptr<PlayerBuddyList> buddyList;
 	scoped_ptr<PlayerInventory> inv;
+	scoped_ptr<PlayerMounts> mounts;
 	scoped_ptr<PlayerPets> pets;
 	scoped_ptr<PlayerQuests> quests;
 	scoped_ptr<PlayerSkills> skills;

commit 1471eff4c296ac7718d508e695bfae333d2a1008

Fixed broken sql in r2198
git-svn-id: svn://70.113.44.214/VanaMirror/trunk@2204 09c1253f-3d09-8e40-9719-4320e56601b5

sql/0060_mount_table.sql
@@ -8,4 +8,4 @@ CREATE TABLE `mounts` (
   KEY `mountid` (`mountid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
-DELETE FROM `items` WHERE FLOOR(`items.itemid` / 10000) = 190;
+DELETE FROM `items` WHERE FLOOR(itemid / 10000) = 190;