8c52b9a5c83206aa6cdc9f1255812cb7f436945c
LazyBui <LazyBui@09c1253f-3d09-8e40-9719-4320e56601b5>
2009-06-10 10:27:42 +0000

    - Ninja Ambush merging
    git-svn-id: svn://70.113.44.214/VanaMirror/trunk@2252 09c1253f-3d09-8e40-9719-4320e56601b5

--- src/ChannelServer/Skills.cpp
+++ src/ChannelServer/Skills.cpp
@@ -180,7 +180,9 @@ void Skills::useSkill(Player *player, PacketReader &packet) {
 		case Jobs::FlameWizard::Seal:
 		case Jobs::Priest::Doom:
 		case Jobs::Hermit::ShadowWeb:
-		case Jobs::NightWalker::ShadowWeb: {
+		case Jobs::NightWalker::ShadowWeb:
+		case Jobs::Shadower::NinjaAmbush:
+		case Jobs::NightLord::NinjaAmbush: {
 			uint8_t mobs = packet.get<int8_t>();
 			for (uint8_t k = 0; k < mobs; k++) {
 				if (Mob *mob = Maps::getMap(player->getMap())->getMob(packet.get<int32_t>())) {
