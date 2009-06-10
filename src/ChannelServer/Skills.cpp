274e22d17b4d4ecda9bf40ce27318d4883e16bd9
LazyBui <LazyBui@09c1253f-3d09-8e40-9719-4320e56601b5>
2009-06-10 01:30:13 +0000

    - Infighter's MP Recovery skill
    git-svn-id: svn://70.113.44.214/VanaMirror/trunk@2238 09c1253f-3d09-8e40-9719-4320e56601b5

--- src/ChannelServer/Skills.cpp
+++ src/ChannelServer/Skills.cpp
@@ -127,6 +127,13 @@ void Skills::useSkill(Player *player, PacketReader &packet) {
 		return;
 	}
 	switch (skillid) {
+		case Jobs::Infighter::MpRecovery: {
+			int16_t modhp = player->getMHp() * Skills::skills[skillid][level].x / 100;
+			int16_t healmp = modhp * Skills::skills[skillid][level].y / 100;
+			player->modifyHp(-modhp);
+			player->modifyMp(healmp);
+			break;
+		}
 		case Jobs::Shadower::Smokescreen: {
 			int16_t x = packet.get<int16_t>();
 			int16_t y = packet.get<int16_t>();
