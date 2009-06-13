1b176a3e8884fb2a923f31a4717a3c797aaa7566
LazyBui <LazyBui@23f3cdc0-e6d1-42be-a0c1-51f4125517bb>
2009-06-13 06:04:15 +0000

    - Minor changes to poison/mist handling
    git-svn-id: https://dev.chiasoft.net:8443/svn/svn_vana/trunk@2281 23f3cdc0-e6d1-42be-a0c1-51f4125517bb

--- src/ChannelServer/Map.cpp
+++ src/ChannelServer/Map.cpp
@@ -399,7 +399,7 @@ void Map::checkMists() {
 
 	for (unordered_map<int32_t, Mob *>::iterator iter = mobs.begin(); iter != mobs.end(); ++iter) {
 		mob = iter->second;
-		if (mob == 0 || mob->hasStatus(StatusEffects::Mob::Poison))
+		if (mob == 0 || mob->hasStatus(StatusEffects::Mob::Poison) || mob->getHp() == 1)
 			continue;
 		for (miter = mists.begin(); miter != mists.end(); ++miter) {
 			mist = miter->second;
