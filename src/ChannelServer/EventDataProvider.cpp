edf70b5d3cc8d8d2a657910fdc799bbde08704cc
LazyBui <LazyBui@23f3cdc0-e6d1-42be-a0c1-51f4125517bb>
2009-06-13 07:27:14 +0000

    - Thanks, make! *80s smile and thumbs up*
    git-svn-id: https://dev.chiasoft.net:8443/svn/svn_vana/trunk@2283 23f3cdc0-e6d1-42be-a0c1-51f4125517bb

--- src/ChannelServer/EventDataProvider.cpp
+++ src/ChannelServer/EventDataProvider.cpp
@@ -21,6 +21,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include "Instances.h"
 #include "Timer/Time.h"
 #include "Timer/Timer.h"
+#include <cstdio>
 #include <functional>
 #include <iostream>
 
