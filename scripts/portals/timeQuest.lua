f3eab56808b7252fbacf28dc34b51e4bb111be9b
LazyBui <LazyBui@09c1253f-3d09-8e40-9719-4320e56601b5>
2009-07-02 02:38:16 +0000

    - 99+% accurate global script for Time Temple portals
    git-svn-id: svn://70.113.44.214/VanaMirror/trunk@2448 09c1253f-3d09-8e40-9719-4320e56601b5

--- scripts/portals/timeQuest.lua
+++ scripts/portals/timeQuest.lua
@@ -18,49 +18,86 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 -- Portals throughout the Time Temple
 
 map = getMap();
+tomapifdone = 0;
 tomap = 0;
+quest = 0;
 
 -- Memory/green area
 if map == 270010100 then
-	tomap = 270010110;
+	quest = 3501;
+	tomapifdone = 270010110;
+	tomap = 270010000;
 elseif map == 270010200 then
-	tomap = 270010210;
+	quest = 3502;
+	tomapifdone = 270010210;
+	tomap = 270010110;
 elseif map == 270010300 then
-	tomap = 270010310;
+	quest = 3503;
+	tomapifdone = 270010310;
+	tomap = 270010210;
 elseif map == 270010400 then
-	tomap = 270010410;
+	quest = 3504;
+	tomapifdone = 270010410;
+	tomap = 270010310;
 elseif map == 270010500 then
-	tomap = 270020000;
+	quest = 3507;
+	tomapifdone = 270020000;
+	tomap = 270010410;
 
 -- Regrets/blue area
 elseif map == 270020100 then
-	tomap = 270020110;
+	quest = 3508;
+	tomapifdone = 270020110;
+	tomap = 270020000;
 elseif map == 270020200 then
-	tomap = 270020210;
+	quest = 3509;
+	tomapifdone = 270020210;
+	tomap = 270020110;
 elseif map == 270020300 then
-	tomap = 270020310;
+	quest = 3510;
+	tomapifdone = 270020310;
+	tomap = 270020210;
 elseif map == 270020400 then
-	tomap = 270020410;
+	quest = 3511;
+	tomapifdone = 270020410;
+	tomap = 270020310;
 elseif map == 270020500 then
-	tomap = 270030000;
+	quest = 3514;
+	tomapifdone = 270030000;
+	tomap = 270020410;
 
 -- Oblivion/red area
 elseif map == 270030100 then
-	tomap = 270030110;
+	quest = 3515;
+	tomapifdone = 270030110;
+	tomap = 270030100;
 elseif map == 270030200 then
+	quest = 3516;
+	tomapifdone = 270030210;
 	tomap = 270030210;
 elseif map == 270030300 then
+	quest = 3517;
+	tomapifdone = 270030310;
 	tomap = 270030310;
 elseif map == 270030400 then
+	quest = 3518;
+	tomapifdone = 270030410;
 	tomap = 270030410;
 elseif map == 270030500 then
-	tomap = 270040000;
+	quest = 3521;
+	tomapifdone = 270040000;
+	tomap = 270030410;
 
 -- Deep palace
 elseif map == 270040000 then
-	tomap = 270040100;
+	quest = 3522;
+	tomapifdone = 270040100;
+	tomap = 270040000;
 end
 
-if tomap ~= 0 then
-	setMap(tomap, "out00");
+if isQuestCompleted(quest) then
+	setMap(tomapifdone, "out00");
+else
+	showMessage("Those who have not received permission cannot walk against the flow of the temple and will return to the previous place.", 5);
+	setMap(tomap);
 end
