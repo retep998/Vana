bfdff861e0cb23c8af3e2bb7a87039a6a8b3061d
LazyBui <LazyBui@23f3cdc0-e6d1-42be-a0c1-51f4125517bb>
2009-07-10 00:21:40 +0000

    - Fixed bug where unusual potions that kill you didn't work
    git-svn-id: https://dev.chiasoft.net:8443/svn/svn_vana/trunk@2468 23f3cdc0-e6d1-42be-a0c1-51f4125517bb

--- src/ChannelServer/Inventory.cpp
+++ src/ChannelServer/Inventory.cpp
@@ -490,9 +490,9 @@ void Inventory::useItem(Player *player, int32_t itemid) {
 		player->modifyMp(item.cons.mp * potency / 100);
 	else
 		player->setMp(player->getMp(), true);
-	if (item.cons.hpr > 0)
+	if (item.cons.hpr != 0)
 		player->modifyHp(item.cons.hpr * (zombie ? (player->getMHp() / 2) : player->getMHp()) / 100);
-	if (item.cons.mpr > 0)
+	if (item.cons.mpr != 0)
 		player->modifyMp(item.cons.mpr * player->getMMp() / 100);
 	if (item.cons.ailment > 0)
 		player->getActiveBuffs()->useDebuffHealingItem(item.cons.ailment);
