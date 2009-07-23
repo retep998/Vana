496d64ae1b3274ed5f6bf747563849d050f485ae
LazyBui <LazyBui@23f3cdc0-e6d1-42be-a0c1-51f4125517bb>
2009-07-23 06:47:21 +0000

    - Added proper Nine Spirit's Egg stuff
    git-svn-id: https://dev.chiasoft.net:8443/svn/svn_vana/branches/62_support@2512 23f3cdc0-e6d1-42be-a0c1-51f4125517bb

---
+++ scripts/instances/horntailEgg.lua
@@ -0,0 +1,70 @@
+--[[
+Copyright (C) 2008-2009 Vana Development Team
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; version 2
+of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+--]]
+function beginInstance()
+	addInstanceMap(240040611);
+end
+
+function playerDeath(playerid)
+
+end
+
+function playerDisconnect(playerid, ispartyleader)
+	finish(false);
+end
+
+function instanceTimerEnd(fromtimer)
+	if getInstancePlayerCount() > 0 then
+		moveAllPlayers(240040610);
+		removeAllInstancePlayers();
+		finish(true);
+	end
+end
+
+function timerEnd(name, fromtimer)
+
+end
+
+function mobDeath(mobid, mapmobid, mapid)
+
+end
+
+function mobSpawn(mobid, mapmobid, mapid)
+
+end
+
+function changeMap(playerid, newmap, oldmap, ispartyleader)
+	finish(false);
+end
+
+function partyDisband(partyid)
+
+end
+
+function partyRemoveMember(partyid, playerid)
+
+end
+
+function finish(fromtimer)
+	g = tonumber(getInstanceVariable("npc"));
+	if g then
+		removeNPC(240040611, g);
+	end
+	if not fromtimer then
+		markForDelete();
+	end
+end
---
+++ scripts/portals/dragonNest.lua
@@ -0,0 +1,31 @@
+--[[
+Copyright (C) 2008-2009 Vana Development Team
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; version 2
+of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+--]]
+-- Nine Spirits Egg portal
+
+if getItemAmount(4001094) > 0 then
+	if not isInstance("horntailEgg") then
+		createInstance("horntailEgg", 5 * 60, true);
+		addInstancePlayer(getID());
+		playPortalSE();
+		setMap(240040611);
+	else
+		showMessage("Someone else is already inside in an attempt to complete the quest. Please try again later.", 5);
+	end
+else
+	showMessage("In order to enter the premise, you'll need to have the Nine Spirit's Egg in possession..", 5);
+end
