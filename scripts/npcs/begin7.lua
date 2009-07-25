762636953f80d5d0d7c057328829847a2a61e8ef
pawitp <pawitp@09c1253f-3d09-8e40-9719-4320e56601b5>
2009-07-25 01:13:39 +0000

    Shanks fix (by xthepersonx)
    git-svn-id: svn://70.113.44.214/VanaMirror/trunk@2528 09c1253f-3d09-8e40-9719-4320e56601b5

--- scripts/npcs/begin7.lua
+++ scripts/npcs/begin7.lua
@@ -17,41 +17,43 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 --]]
 -- Shanks
 
-addText("Take this ship and you'll head off to a bigger continent.#e For 150 mesos#n, I'll take you to #bVictoria Island#k");
-addText(". The thing is, once you leave this place, you can't ever come back. What do you think? Do you want to go to Victoria Island?");
+if getItemAmount(4031801) > 0 then
+	lucas = true;
+end
+
+addText("Take this ship and you'll head off to a bigger continent.#e For 150 mesos#n, I'll take you to #bVictoria Island#k. The thing is, once you leave this place, you can't ever come back. What do you think? Do you want to go to Victoria Island?");
 yes = askYesNo();
 
 if yes == 1 then
-	if getItemAmount(4031801) > 0 then
-		lucas = true;
-		
-		addText("Okay, now give me 150 mesos... Hey, what's that? Is that the recommendation letter from Lucas, the chief of Amherst? Hey, you should have told me you had this. I, Shanks, recognize greatness when I see one, and since you have been recommended by Lucas, I see that you have a great, great potential as an adventurer. No way would I charge you for this trip!");
-		sendNext();
-		
-		if getLevel() >= 7 then
-			if lucas then
-				addText("Since you have the recommendation letter, I won't charge you for this. Alright, buckle up, because we're going to head to Victoria Island right now, and it might get a bit turbulent!!");
-				sendBackNext();
-			elseif getMesos() >= 150 then
-				addText("Awesome! #e150 mesos#n accepted! Alright, off to Victoria Island!");
-				sendNext();
-				
-				giveMesos(-150);
-				setMap(104000000);
-			else
-				addText("What? You're telling me you wanted to go without any money? You're one weirdo...");
-				sendOK();
-			end
+	if getLevel() >= 7 then
+		if lucas then
+			addText("Okay, now give me 150 mesos... Hey, what's that? Is that the recommendation letter from Lucas, the chief of Amherst? Hey, you should have told me you had this. I, Shanks, recognize greatness when I see one, and since you have been recommended by Lucas, I see that you have a great, great potential as an adventurer. No way would I charge you for this trip!");
+			sendNext();
+
+			addText("Since you have the recommendation letter, I won't charge you for this. Alright, buckle up, because we're going to head to Victoria Island right now, and it might get a bit turbulent!!");
+			sendBackNext();
+
+			giveItem(4031801, -1);
+			setMap(104000000);
+		elseif getMesos() >= 150 then
+			addText("Bored of this place? Here... Give me 150 mesos first...");
+			sendNext();
+
+			addText("Awesome! #e150 mesos#n accepted! Alright, off to Victoria Island!");
+			sendNext();
+
+			giveMesos(-150);
+			setMap(104000000);
 		else
-			addText("Let's see... I don't think you are strong enough. You'll have to be at least #bLevel 7#k ");
-			addText("to go to Victoria Island.");
-			sendOK();
+			addText("What? You're telling me you wanted to go without any money? You're one weirdo...");
+			sendBackNext();
 		end
 	else
-		addText("Bored of this place? Here... Give me 150 mesos first...");
+		addText("Let's see... I don't think you are strong enough. You'll have to be at least #bLevel 7#k ");
+		addText("to go to Victoria Island.");
 		sendNext();
 	end
 else
 	addText("Hmm... I guess you still have things to do here?");
-	sendOK();
+	sendNext();
 end
