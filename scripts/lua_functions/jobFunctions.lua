4cf2f53bc383e24301994f6a48a21cca1b0c6ff7
LazyBui <LazyBui@09c1253f-3d09-8e40-9719-4320e56601b5>
2009-07-26 02:07:18 +0000

    - Basic 3rd job battle instances and entry NPC
    git-svn-id: svn://70.113.44.214/VanaMirror/trunk@2543 09c1253f-3d09-8e40-9719-4320e56601b5

---
+++ scripts/lua_functions/jobFunctions.lua
@@ -0,0 +1,39 @@
+--[[
+Copyright (C) 2008-2009 Vana Development Team
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; version 2
+of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+--]]
+-- A subset of Lua functions that helps with common job-related tasks
+
+function getJobLine()
+	local g = getJob();
+	g = (g - (g % 100)) / 100;
+	return g;
+end
+
+function getJobTrack()
+	local g = getJob();
+	g = ((g - (g % 10)) / 10) % 10;
+	return g;
+end
+
+function getJobProgression()
+	local g = getJob() % 10;
+	return g;
+end
+
+function isCygnus()
+	return getJob() >= 1000;
+end
