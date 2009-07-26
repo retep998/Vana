e8e55bae4e4cc99772c585ec2b678f944b16c3ce
LazyBui <LazyBui@09c1253f-3d09-8e40-9719-4320e56601b5>
2009-07-26 00:54:08 +0000

    - Added 3rd job room exit script
    git-svn-id: svn://70.113.44.214/VanaMirror/trunk@2542 09c1253f-3d09-8e40-9719-4320e56601b5

---
+++ scripts/npcs/3jobExit.lua
@@ -0,0 +1,38 @@
+--[[
+Copyright (C) 2008-2009 Vana Development Team
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; version 2
+of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+--]]
+-- Sparkling Crystal
+
+addText("You can use the Sparkling Crystal to go back to the real world. Are you sure you want to go back?");
+yes = askYesNo();
+
+if yes == 1 then
+	tomap = 0;
+	m = getMap();
+	if m == 108010101 then
+		tomap = 100000000;
+	elseif m == 108010201 then
+		tomap = 101000000;
+	elseif m == 108010301 then
+		tomap = 102000000;
+	elseif m == 108010401 then
+		tomap = 103000000;
+	elseif m == 108010501 then
+		tomap = 120000000;
+	end
+	setMap(tomap);
+end
