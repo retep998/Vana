604fdbc1a4ff2453962c6d669051d5281b08d2c8
LazyBui <LazyBui@23f3cdc0-e6d1-42be-a0c1-51f4125517bb>
2009-07-26 02:08:57 +0000

    - Split Inventory from InventoryHandler
    git-svn-id: https://dev.chiasoft.net:8443/svn/svn_vana/trunk@2544 23f3cdc0-e6d1-42be-a0c1-51f4125517bb

--- src/ChannelServer/Inventory.h
+++ src/ChannelServer/Inventory.h
@@ -30,26 +30,11 @@ class PacketReader;
 struct Item;
 
 namespace Inventory {
-	void useShop(Player *player, PacketReader &packet);
-	void useStorage(Player *player, PacketReader &packet);
-	void itemMove(Player *player, PacketReader &packet);
 	int16_t addItem(Player *player, Item *item, bool is = false);
 	void addNewItem(Player *player, int32_t itemid, int16_t amount);
 	void takeItem(Player *player, int32_t itemid, uint16_t howmany);
-	void useItem(Player *player, PacketReader &packet);
 	void useItem(Player *player, int32_t itemid);
-	void cancelItem(Player *player, PacketReader &packet);
-	void useSkillbook(Player *player, PacketReader &packet);
 	void takeItemSlot(Player *player, int8_t inv, int16_t slot, int16_t amount, bool takeStar = false);
-	void useChair(Player *player, PacketReader &packet);
-	void useItemEffect(Player *player, PacketReader &packet);
-	void handleChair(Player *player, PacketReader &packet);
-	void useSummonBag(Player *player, PacketReader &packet);
-	void useReturnScroll(Player *player, PacketReader &packet);
-	void useScroll(Player *player, PacketReader &packet);
-	void useCashItem(Player *player, PacketReader &packet);
-	void handleRockFunctions(Player *player, PacketReader &packet);
-	bool handleRockTeleport(Player *player, int8_t type, int32_t itemid, PacketReader &packet);
 };
 
 #endif
