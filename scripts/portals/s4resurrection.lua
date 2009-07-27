4847d7356b6518e9cdc53ff94f4176e5c6f1f3c7
LazyBui <LazyBui@09c1253f-3d09-8e40-9719-4320e56601b5>
2009-07-27 19:33:49 +0000

    - Resurrection quest scripts
    git-svn-id: svn://70.113.44.214/VanaMirror/trunk@2556 09c1253f-3d09-8e40-9719-4320e56601b5

---
+++ scripts/portals/s4resurrection.lua
@@ -0,0 +1,35 @@
+--[[
+Copyright (C) 2008-2009 Vana Development Team
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; version 2
+of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+--]]
+-- Portal to Bishop Resurrection quest - cauldron thing
+
+if isQuestActive(6132) then
+	if getItemAmount(4161017) > 0 then
+		showMessage("You can't enter as you already have Caron's Note.", 5);
+	else
+		if not isInstance("resurrectionMix4th") then
+			createInstance("resurrectionMix4th", 5 * 60, true);
+			addInstancePlayer(getID());
+			playPortalSE();
+			setMap(923000100);
+		else
+			showMessage("You feel someone is out there in the cold cave.", 5);
+		end
+	end
+else
+	showMessage("You can't enter because of cold energy from the cave.", 5);
+end
