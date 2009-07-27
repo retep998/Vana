9c9cb4fe30915869cfb7b16d6b0ae2fc6645af4b
LazyBui <LazyBui@23f3cdc0-e6d1-42be-a0c1-51f4125517bb>
2009-07-27 17:48:09 +0000

    - Added Snipe/Concentrate scripts  - Fixed slight error in Rush script
    git-svn-id: https://dev.chiasoft.net:8443/svn/svn_vana/trunk@2554 23f3cdc0-e6d1-42be-a0c1-51f4125517bb

--- scripts/npcs/s4snipe.lua
+++ scripts/npcs/s4snipe.lua
@@ -15,49 +15,55 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 --]]
--- Portal to 4th job warrior quest
+-- Insignificant Being
 
 dofile("scripts/lua_functions/jobFunctions.lua");
 
-if isQuestActive(6110) then
+if isQuestActive(6108) then
 	if getPartyID() == 0 then
-		showMessage("You don't have a  party. You can challenge with party.", 5);
+		addText("You don't have a  party. You can challenge with party.");
+		sendOK();
 	elseif isPartyLeader() then
 		members = getAllPartyPlayerIDs();
 		if #members ~= 2 then
-			showMessage("You can make a quest when you have a party with two. Please make your party with two members.", 5);
+			addText("You can make a quest when you have a party with two. Please make your party with two members.");
+			sendOK();
 		else
 			if not isPartyInLevelRange(120, 200) then
-				showMessage("There is a character among your party whose level is not eligible. You should be level 120 above. Please adjust level.", 5);
+				addText("There is a character among your party whose level is not eligible. You should be level 120 above. Please adjust level.");
+				sendOK();
 			else
 				memberctr = 0
 				for i = 1, #members do
 					member = members[i];
 					if setPlayer(member) then
-						if getJobLine() == 1 and getJobProgression() == 2 then -- Only want 4th job warriors
+						if getJobLine() == 3 and getJobProgression() == 2 then -- Only want 4th job bowmen
 							memberctr = memberctr + 1;
 						end
 						revertPlayer();
 					end
 				end
 				if memberctr ~= 2 then
-					showMessage("You can't enter. Your party member's job is not Warrior or Your party doesn't consist of two members.", 5);
+					addText("You can't enter. Your party member's job is not Bow Master or Marksman or Your party doesn't consist of two members.");
+					sendOK();
 				else
-					if not isInstance("rush4th") then
-						createInstance("rush4th", 20 * 60, true);
+					if not isInstance("snipe4th") then
+						createInstance("snipe4th", 20 * 60, true);
 						addInstancePlayer(getID());
 						addInstanceParty(getPartyID());
-						playPortalSE();
-						setMap(910500100);
+						setMap(910500000);
 					else
-						showMessage("Other parties are challenging on quest clear now. Try again later.", 5);
+						addText("Other parties are challenging on quest clear now. Try again later.");
+						sendOK();
 					end
 				end
 			end
 		end
 	else
-		showMessage("Party leader consisting of two Warriors can decide to enter.", 5);
+		addText("After two Bowmans who made 4th advancement make a party, party leader can take to me.");
+		sendOK();
 	end
 else
-	showMessage("You can't enter sealed place.", 5);
+	addText("I don't know what you're talking about.");
+	sendOK();
 end
