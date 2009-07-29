f8f1b9b04fff32cee273abc5d3d6b56a4c225f57
LazyBui <LazyBui@23f3cdc0-e6d1-42be-a0c1-51f4125517bb>
2009-07-29 04:53:17 +0000

    - Assassinate quest scripts
    git-svn-id: https://dev.chiasoft.net:8443/svn/svn_vana/trunk@2567 23f3cdc0-e6d1-42be-a0c1-51f4125517bb

---
+++ scripts/portals/s4hitman.lua
@@ -0,0 +1,36 @@
+--[[
+Copyright (C) 2008-2009 Vana Development Team
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; version 2
+of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+--]]
+-- Assassinate quest
+
+if isQuestActive(6201) then
+	if getItemAmount(4031452) == 0 then
+		if not isInstance("assassinate4th") then
+			createInstance("assassinate4th", 20 * 60, true);
+			addInstancePlayer(getID());
+			playPortalSE();
+			maps = {910200000, 910200001, 910200002};
+			setMap(maps[getRandomNumber(#maps)]);
+		else
+			showMessage("Other characters are on request. You can't enter.", 5);
+		end
+	else
+		showMessage("Shawn's request is completed. You don't need to go in again.", 5);
+	end
+else
+	showMessage("You can't go. You didn't get Shawn's request.", 5);
+end
