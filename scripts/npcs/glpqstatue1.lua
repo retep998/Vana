c64cf897403d3a52c70e57c9c6a5bd040344b1b9
LazyBui <LazyBui@09c1253f-3d09-8e40-9719-4320e56601b5>
2009-07-31 08:19:35 +0000

    - Some simple Crimsonwood Keep scripts
    git-svn-id: svn://70.113.44.214/VanaMirror/trunk@2584 09c1253f-3d09-8e40-9719-4320e56601b5

---
+++ scripts/npcs/glpqstatue1.lua
@@ -0,0 +1,21 @@
+--[[
+Copyright (C) 2008-2009 Vana Development Team
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; version 2
+of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+--]]
+-- Crimsonwood Keep statue
+
+addText("One of the only known Holy Archers, Lockewood is the one of the Keep's most famous heroes; both feared and respected for his ¡°Genesis Arrow¡± and ¡°Doom Phoenix¡±. Of particular note is his custom white and gold battle garb, said to be blessed by a powerful goddess. His impeccable aim was tremendously accurate over long distances. He once struck down six Typhons from the Valley of Heroes.");
+sendOK();
---
+++ scripts/npcs/glpqstatue4.lua
@@ -0,0 +1,21 @@
+--[[
+Copyright (C) 2008-2009 Vana Development Team
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; version 2
+of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+--]]
+-- Crimsonwood Keep statue
+
+addText("Long ago, a strange warrior washed upon the shores of Masteria.  This being claimed to be a member of a mysterious band of warriors that used claw like weapons and projectile-based artillery to defeat foes.   Known as ¡°Steel Fist Jack¡±, his cunning and trickery in battle was devilishly effective.  He eventually built a boat and left the Keep, in search of his former crew and captain.");
+sendOK();
