7e2890c29d4011552bfecdf33457ade0494717bd
doyos <doyos@09c1253f-3d09-8e40-9719-4320e56601b5>
2009-08-17 23:29:13 +0000

    Fixed compilation issue on Mac OS X (complaint about setw not being a part of std)
    git-svn-id: svn://70.113.44.214/VanaMirror/trunk@2650 09c1253f-3d09-8e40-9719-4320e56601b5

--- src/Common/BuffDataProvider.cpp
+++ src/Common/BuffDataProvider.cpp
@@ -21,6 +21,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include "InitializeCommon.h"
 #include "ItemDataProvider.h"
 #include <iostream>
+#include <iomanip>
 
 using Initializing::outputWidth;
 
