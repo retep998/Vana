b007871a1b36952db0962ba1d2c50234ea1a2a53
doyos <doyos@23f3cdc0-e6d1-42be-a0c1-51f4125517bb>
2009-08-23 10:57:00 +0000

    Removed outdated reference to sendSimple in flymindraco.lua
    git-svn-id: https://dev.chiasoft.net:8443/svn/svn_vana/trunk@2670 23f3cdc0-e6d1-42be-a0c1-51f4125517bb

--- scripts/npcs/flyminidraco.lua
+++ scripts/npcs/flyminidraco.lua
@@ -19,7 +19,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 addText("If you had wings, I'm sure you could go there.  But, that alone won't be enough.  If you want to fly though the wind that's sharper than a blade, you'll need tough scales as well.  I'm the only Halfling left that knows the way back... If you want to go there, I can transform you.  No matter what you are, for this moment, you will become a #bDragon#k...\r\n");
 addText("#L0##bI want to become a dragon.#k#l");
-sendSimple();
+askChoice();
 
 useItem(2210016);
 setMap(200090500);
