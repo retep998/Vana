debadb7ab8ad8aaf7d5f663ef0e02bfe902206d1
doyos <doyos@09c1253f-3d09-8e40-9719-4320e56601b5>
2009-10-16 05:43:24 +0000

    Fixed Guild.h for compiling on non-Windows platforms.
    git-svn-id: svn://70.113.44.214/VanaMirror/trunk@2765 09c1253f-3d09-8e40-9719-4320e56601b5

--- src/WorldServer/Guild.h
+++ src/WorldServer/Guild.h
@@ -15,13 +15,13 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
-
 #ifndef GUILD_H
 #define GUILD_H
 
 #include "GuildBbs.h"
 #include "Players.h"
 #include "Types.h"
+#include <boost/array.hpp>
 #include <boost/scoped_ptr.hpp>
 #include <boost/tr1/unordered_map.hpp>
 #include <boost/utility.hpp>
