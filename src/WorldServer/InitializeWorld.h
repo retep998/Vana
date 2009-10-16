1708a60f038252af9b4edd8ab48864cc091633d1
LazyBui <LazyBui@09c1253f-3d09-8e40-9719-4320e56601b5>
2009-10-16 19:42:20 +0000

    - Separated guild loading from WorldServer, it should only load data relevant to the WorldServer itself
    git-svn-id: svn://70.113.44.214/VanaMirror/trunk@2768 09c1253f-3d09-8e40-9719-4320e56601b5

---
+++ src/WorldServer/InitializeWorld.h
@@ -0,0 +1,31 @@
+/*
+Copyright (C) 2008-2009 Vana Development Team
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; version 2
+of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+#ifndef INITIALIZEWORLD_H
+#define INITIALIZEWORLD_H
+
+#include "Types.h"
+
+namespace Initializing {
+	void loadData();
+	void loadPostAssignment(int16_t worldId);
+	void loadGuilds(int16_t worldId);
+	void loadAlliances(int16_t worldId);
+	void assignPlayers(int16_t worldId);
+}
+
+#endif
