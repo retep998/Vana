17483a78d5e8fd60a646896a64f7fe850996e967
LazyBui <LazyBui@09c1253f-3d09-8e40-9719-4320e56601b5>
2009-10-22 05:09:11 +0000

    - One last make fix
    git-svn-id: svn://70.113.44.214/VanaMirror/trunk@2788 09c1253f-3d09-8e40-9719-4320e56601b5

--- src/WorldServer/PlayerDataProvider.h
+++ src/WorldServer/PlayerDataProvider.h
@@ -21,6 +21,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include "GameObjects.h"
 #include "Types.h"
 #include <boost/tr1/unordered_map.hpp>
+#include <boost/utility.hpp>
 #include <string>
 
 using std::string;
