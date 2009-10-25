4f32581320b3bed0870c0bea5c490f41379a30f5
doyos <doyos@23f3cdc0-e6d1-42be-a0c1-51f4125517bb>
2009-10-25 11:32:53 +0000

    Added Inventories::EquippedSlots constant to save future headaches when Nexon adds more equipped item slots.
    git-svn-id: https://dev.chiasoft.net:8443/svn/svn_vana/trunk@2803 23f3cdc0-e6d1-42be-a0c1-51f4125517bb

--- src/LoginServer/LoginPacketHelper.cpp
+++ src/LoginServer/LoginPacketHelper.cpp
@@ -17,6 +17,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 #include "LoginPacketHelper.h"
 #include "Characters.h"
+#include "GameConstants.h"
 #include "PacketCreator.h"
 
 void LoginPacketHelper::addCharacter(PacketCreator &packet, const Character &charc) {
@@ -55,7 +56,7 @@ void LoginPacketHelper::addCharacter(PacketCreator &packet, const Character &cha
 	packet.add<int32_t>(charc.eyes);
 	packet.add<int8_t>(1);
 	packet.add<int32_t>(charc.hair);
-	int32_t equips[51][2] = {0};
+	int32_t equips[Inventories::EquippedSlots][2] = {0};
 	for (int16_t i = 0; i < (int16_t) charc.equips.size(); i++) {
 		int16_t slot = -charc.equips[i].slot;
 		if (slot > 100)
@@ -73,7 +74,7 @@ void LoginPacketHelper::addCharacter(PacketCreator &packet, const Character &cha
 			equips[slot][0] = charc.equips[i].id;
 		}
 	}
-	for (int8_t i = 0; i < 50; i++) { // Shown items
+	for (int8_t i = 0; i < Inventories::EquippedSlots; i++) { // Shown items
 		if (equips[i][0] > 0) {
 			packet.add<int8_t>(i);
 			if (i == 11 && equips[i][1] > 0) // Normal weapons always here
@@ -83,7 +84,7 @@ void LoginPacketHelper::addCharacter(PacketCreator &packet, const Character &cha
 		}
 	}
 	packet.add<int8_t>(-1);
-	for (int8_t i = 0; i < 50; i++) { // Covered items
+	for (int8_t i = 0; i < Inventories::EquippedSlots; i++) { // Covered items
 		if (equips[i][1] > 0 && i != 11) {
 			packet.add<int8_t>(i);
 			packet.add<int32_t>(equips[i][1]);
