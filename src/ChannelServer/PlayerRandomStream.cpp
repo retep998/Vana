3fa8c960c3ff96b11905b1a01285a27724964e1c
LazyBui <LazyBui@09c1253f-3d09-8e40-9719-4320e56601b5>
2009-11-01 21:25:14 +0000

    - Modified some properties of the Tausworthe generator to be more standards-conforming (the changes are mathematically equivalent on unsigned integers, which the formula requires)
    git-svn-id: svn://70.113.44.214/VanaMirror/trunk@2838 09c1253f-3d09-8e40-9719-4320e56601b5

--- src/ChannelServer/PlayerRandomStream.cpp
+++ src/ChannelServer/PlayerRandomStream.cpp
@@ -25,28 +25,28 @@ m_player(p)
 {
 }
 
-void PlayerRandStream::reset(int32_t seed1, int32_t seed2, int32_t seed3) {
+void PlayerRandStream::reset(uint32_t seed1, uint32_t seed2, uint32_t seed3) {
 	m_seed1 = seed1 | 0x100000;
 	m_seed2 = seed2 | 0x1000;
 	m_seed3 = seed3 | 0x10;
 }
 
-int32_t PlayerRandStream::next() {
-	m_seed1 = ((m_seed1 & 0xFFFFFFFE) << 12) ^ ((m_seed1 & 0x7FFC0 ^ (m_seed1 >> 13)) >> 6);
-	m_seed2 = 16 * (m_seed2 & 0xFFFFFFF8) ^ (((m_seed2 >> 2) ^ m_seed2 & 0x3F800000) >> 23);
-	m_seed3 = ((m_seed3 & 0xFFFFFFF0) << 17) ^ (((m_seed3 >> 3) ^ m_seed3 & 0x1FFFFF00) >> 8);
+uint32_t PlayerRandStream::next() {
+	m_seed1 = ((m_seed1 & 0xFFFFFFFE) << 12) ^ (((m_seed1 << 13) ^ m_seed1) >> 19);
+	m_seed2 = ((m_seed2 & 0xFFFFFFF8) << 4) ^ (((m_seed2 << 2) ^ m_seed2) >> 25);
+	m_seed3 = ((m_seed3 & 0xFFFFFFF0) << 17) ^ (((m_seed3 << 3) ^ m_seed3) >> 11);
 	return (m_seed1 ^ m_seed2 ^ m_seed3);
 }
 
 void PlayerRandStream::connectData(PacketCreator &packet) {
 	// Depending on how this works, may need to simply send m_seed1/etc. after reset call
-	int32_t s1 = Randomizer::Instance()->randInt();
-	int32_t s2 = Randomizer::Instance()->randInt();
-	int32_t s3 = Randomizer::Instance()->randInt();
+	uint32_t s1 = Randomizer::Instance()->randInt();
+	uint32_t s2 = Randomizer::Instance()->randInt();
+	uint32_t s3 = Randomizer::Instance()->randInt();
 
 	reset(s1, s2, s3);
 
-	packet.add<int32_t>(s1);
-	packet.add<int32_t>(s2);
-	packet.add<int32_t>(s3);
+	packet.add<uint32_t>(s1);
+	packet.add<uint32_t>(s2);
+	packet.add<uint32_t>(s3);
 }
