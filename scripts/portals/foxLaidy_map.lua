3cb65a150f52d6443b887f5c25ee57edf2eca28d
LazyBui <LazyBui@23f3cdc0-e6d1-42be-a0c1-51f4125517bb>
2009-11-10 16:29:44 +0000

    - Portal so people don't get stuck
    git-svn-id: https://dev.chiasoft.net:8443/svn/svn_vana/trunk@2851 23f3cdc0-e6d1-42be-a0c1-51f4125517bb

---
+++ scripts/portals/foxLaidy_map.lua
@@ -0,0 +1,21 @@
+--[[
+Copyright (C) 2008-2009 Vana Development Team
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; version 2
+of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+--]]
+-- KFT portal at the entrance to Fox Ridge (quest portal, I think)
+
+playPortalSE();
+setMap(222010200, "east00");
