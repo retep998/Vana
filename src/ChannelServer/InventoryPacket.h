3d38c4555c02fd10ea6db5cd9f2c33e789281341
LazyBui <LazyBui@23f3cdc0-e6d1-42be-a0c1-51f4125517bb>
2010-01-13 19:49:07 +0000

    - Diamondo25's patch for proper triple megaphones and .. this congratulatory song thing
    git-svn-id: https://dev.chiasoft.net:8443/svn/svn_vana/trunk@2888 23f3cdc0-e6d1-42be-a0c1-51f4125517bb

--- src/ChannelServer/InventoryPacket.h
+++ src/ChannelServer/InventoryPacket.h
@@ -42,7 +42,8 @@ namespace InventoryPacket {
 	void showMegaphone(Player *player, const string &msg);
 	void showSuperMegaphone(Player *player, const string &msg, bool whisper = false);
 	void showMessenger(Player *player, const string &msg, const string &msg2, const string &msg3, const string &msg4, unsigned char *displayInfo, int32_t displayInfo_size, int32_t itemid);
-	void showItemMegaphone(Player *player, const string &msg, bool whisper = false, Item *item = 0);
+	void showItemMegaphone(Player *player, const string &msg, bool whisper = false, Item *item = nullptr);
+	void showTripleMegaphone(Player *player, int8_t lines, const string &line1, const string &line2, const string &line3, bool whisper);
 	void useItemEffect(Player *player, int32_t itemid);
 	void updateSlots(Player *player, int8_t inventory, int8_t slots);
 	void sendRockUpdate(Player *player, int8_t mode, int8_t type, const vector<int32_t> &maps);
@@ -54,5 +55,6 @@ namespace InventoryPacket {
 	void sendHulkSmash(Player *player, int16_t slot, Item *hammered);
 	void sendHammerUpdate(Player *player);
 	void sendChalkboardUpdate(Player *player, const string &msg = "");
+	void playCashSong(int32_t map, int32_t itemid, const string &playername);
 	void blankUpdate(Player *player);
 };
