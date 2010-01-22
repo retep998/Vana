92909d2ade0aae605d5583cd4b4024f2249859bd
LazyBui <LazyBui@23f3cdc0-e6d1-42be-a0c1-51f4125517bb>
2010-01-22 17:16:30 +0000

    - Diamondo25's batch of Amoria NPCs
    git-svn-id: https://dev.chiasoft.net:8443/svn/svn_vana/trunk@2891 23f3cdc0-e6d1-42be-a0c1-51f4125517bb

---
+++ scripts/npcs/EngageRing.lua
@@ -0,0 +1,31 @@
+--[[
+Copyright (C) 2008-2010 Vana Development Team
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; version 2
+of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+--]]
+-- Moony
+
+addText("Have you found true love? If so, I can make you a ring worthy of your devotion...\r\n");
+addText("#b#L0#I would like to make an engagement ring for my lover.\r\n");
+addText("#L1#I want an annulment.#l#k");
+value = askChoice();
+
+if value == 0 then
+	addText("Looks like you're not quite done. If you want that ring, you'd better hurry and get me the following all materials to make an engagement ring. I need you to bring me #b4 Proof of Love#k from Nana.");
+	sendNext();
+elseif value == 1 then
+	addText("It looks like you haven't married yet. I don't have anything to talk to you about this.");
+	sendOk();
+end
