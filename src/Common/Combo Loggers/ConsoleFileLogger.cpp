8a184e6df6e83d7f88cb5ea0b0af1bcf76d2617c
LazyBui <LazyBui@23f3cdc0-e6d1-42be-a0c1-51f4125517bb>
2010-02-22 01:10:48 +0000

    - Expanded loggers to include the ability to log to more than one location  - Suggestion by Diamondo25
    git-svn-id: https://dev.chiasoft.net:8443/svn/svn_vana/trunk@2914 23f3cdc0-e6d1-42be-a0c1-51f4125517bb

---
+++ src/Common/Combo Loggers/ConsoleFileLogger.cpp
@@ -0,0 +1,31 @@
+/*
+Copyright (C) 2008-2010 Vana Development Team
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; version 2
+of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+#include "ConsoleFileLogger.h"
+#include "Database.h"
+
+ConsoleFileLogger::ConsoleFileLogger(const string &filename, const string &format, const string &timeFormat, int16_t serverType, size_t bufferSize) :
+Logger(format, timeFormat, serverType)
+{
+	m_console.reset(new ConsoleLogger(format, timeFormat, serverType));
+	m_file.reset(new FileLogger(filename, format, timeFormat, serverType, bufferSize));
+}
+
+void ConsoleFileLogger::log(LogTypes::LogTypes type, const string &identifier, const string &message) {
+	getConsoleLogger()->log(type, identifier, message);
+	getFileLogger()->log(type, identifier, message);
+}
---
+++ src/Common/Combo Loggers/ConsoleSqlLogger.cpp
@@ -0,0 +1,31 @@
+/*
+Copyright (C) 2008-2010 Vana Development Team
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; version 2
+of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+#include "ConsoleSqlLogger.h"
+#include "Database.h"
+
+ConsoleSqlLogger::ConsoleSqlLogger(const string &format, const string &timeFormat, int16_t serverType, size_t bufferSize) :
+Logger(format, timeFormat, serverType)
+{
+	m_sql.reset(new SqlLogger(format, timeFormat, serverType, bufferSize));
+	m_console.reset(new ConsoleLogger(format, timeFormat, serverType));
+}
+
+void ConsoleSqlLogger::log(LogTypes::LogTypes type, const string &identifier, const string &message) {
+	getSqlLogger()->log(type, identifier, message);
+	getConsoleLogger()->log(type, identifier, message);
+}
