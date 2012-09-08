1ae1a1c263e12488ccfab3f758bf1f530414dd24
LazyBui <LazyBui@23f3cdc0-e6d1-42be-a0c1-51f4125517bb>
2012-09-08 02:13:06 +0000

    - Adding quest for Hero's Will  - Fixing a small discrepancy between the Griffey portal and the !map griffey command
    git-svn-id: https://dev.chiasoft.net:8443/svn/svn_vana/branches/buis_vana_overhaul@3231 23f3cdc0-e6d1-42be-a0c1-51f4125517bb

---
+++ scripts/instances/herosWill.lua
@@ -0,0 +1,62 @@
+--[[
+Copyright (C) 2008-2012 Vana Development Team
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; version 2
+of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+--]]
+
+function beginInstance()
+	addInstanceMap(923000000);
+end
+
+function playerDisconnect(playerId, isPartyLeader)
+	finish(playerId);
+end
+
+function instanceTimerEnd(fromTimer)
+	if getInstancePlayerCount() > 0 then
+		finish2();
+	end
+end
+
+function changeMap(playerId, newMap, oldMap, isPartyLeader)
+	if not isInstanceMap(newMap) then
+		finish(playerId);
+	end
+end
+
+function partyDisband(partyId)
+	finish2();
+end
+
+function partyRemoveMember(partyId, playerId)
+	finish2();
+end
+
+function finish(playerId)
+	removeInstancePlayer(playerId);
+	finish2();
+	markForDelete();
+end
+
+function finish2()
+	moveAllPlayers(230040001);
+	removeAllInstancePlayers();
+end
+
+function playerDeath(playerId) end
+function timerEnd(name, fromTimer) end
+function mobDeath(mobId, mapMobId, mapId) end
+function mobSpawn(mobId, mapMobId, mapId) end
+function friendlyHit(mobId, mapMobId, mapId, hp, maxHp) end
---
+++ scripts/npcs/s4common2.lua
@@ -0,0 +1,74 @@
+--[[
+Copyright (C) 2008-2012 Vana Development Team
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; version 2
+of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+--]]
+-- Carta
+
+dofile("scripts/lua_functions/jobFunctions.lua");
+dofile("scripts/lua_functions/partyQuestHelper.lua");
+
+if isQuestActive(6301) then
+	if getPartyId() ~= 0 then
+		if getItemAmount(4031472) < 40 then
+			if getItemAmount(4000175) > 0 then
+				party = getAllPartyPlayerIds();
+				if getPartyId() == 0 then
+					addText("You don't have a  party. You can challenge with party.");
+					sendOK();
+				else
+					fail = false;
+					for i = 1, #party do
+						p = party[i];
+						if setPlayer(p) then
+							if getJobProgression() ~= 2 then
+								fail = true;
+							end
+							revertPlayer();
+						end
+						if fail then break; end
+					end
+
+					if fail then
+						addText("You can't enter if anyone in your party hasn't make 4th job advancement.");
+						sendOK();
+					else
+						if not isInstance("herosWill") then
+							giveItem(4000175, -1);
+							createInstance("herosWill", 5 * 60, true);
+							addPartyMembersToInstance();
+							moveLocalPartyMembersToMap(923000000);
+						else
+							addText("Other parties are challenging on quest clear now. Try again later.");
+							sendOk();
+						end
+					end
+				end
+			else
+				addText("Without #b#t4000175##k, you can't enter Cracked Dimension.");
+				sendOk();
+			end
+		else
+			addText("If you have 40 #b#t4031472##k, you need no more.");
+			sendOk();
+		end
+	else
+		addText("Only party leader can apply to enter. Please get your representative to talk to me.");
+		sendOk();
+	end
+else
+	addText("Cracked Dimension? Where did you hear that?");
+	sendOk();
+end
