e67dfd5f06015cc0beaf6294326efd53362860ea
LazyBui <LazyBui@09c1253f-3d09-8e40-9719-4320e56601b5>
2013-01-29 02:29:07 +0000

    - Modifying CustomFunctions so it has a ChatCommand defined by default (also, surprise copyright years)
    git-svn-id: svn://70.113.44.214/VanaMirror/branches/buis_vana_overhaul@3259 09c1253f-3d09-8e40-9719-4320e56601b5

--- src/ChannelServer/CustomFunctions.cpp
+++ src/ChannelServer/CustomFunctions.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2008-2012 Vana Development Team
+Copyright (C) 2008-2013 Vana Development Team
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
@@ -20,4 +20,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 void CustomFunctions::initialize(CommandListType &commandList) {
 	// Define any custom functions you may have here, just like ChatHandlerFunctions::initialize
+	ChatCommand command;
+
+
 }
