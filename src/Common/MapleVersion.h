c65ed4038a3548c26bafbcdf122057daa91f5b95
LazyBui <LazyBui@09c1253f-3d09-8e40-9719-4320e56601b5>
2013-11-30 05:43:55 +0000

    Rearranging MapleVersion.h a bit (so you can do things like MAPLE_LOCALE == MAPLE_LOCALE_BRAZIL, makes it a little easier to target specific versions) Upgrading to VS2013 Removing a hack because VS2012's implementation of std::thread was API-incompatible with the standard (while VS2013 fixes that right up)
    git-svn-id: svn://70.113.44.214/VanaMirror/branches/buis_vana_overhaul@3273 09c1253f-3d09-8e40-9719-4320e56601b5

--- src/Common/MapleVersion.h
+++ src/Common/MapleVersion.h
@@ -22,58 +22,65 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 using std::string;
 
-#define MAPLE_LOCALE "global"
-// Used for MCDB verification but can also be used in combination with MAPLE_VERSION
+#define MAPLE_LOCALE_GLOBAL 0x08
+#define MAPLE_LOCALE_KOREA 0x01
+#define MAPLE_LOCALE_JAPAN 0x03
+#define MAPLE_LOCALE_CHINA 0x05
+#define MAPLE_LOCALE_SEA 0x07
+#define MAPLE_LOCALE_THAILAND 0x07
+#define MAPLE_LOCALE_EUROPE 0x09
+#define MAPLE_LOCALE_BRAZIL 0x09
+#define MAPLE_LOCALE_TAIWAN 0x3C
+
+#define MAPLE_LOCALE_KOREA_TEST 0x02
+#define MAPLE_LOCALE_CHINA_TEST 0x04
+#define MAPLE_LOCALE_TAIWAN_TEST 0x06
+#define MAPLE_LOCALE_GLOBAL_TEST 0x05
+
+// MCDB constants - used for verification
+#define MAPLE_LOCALE_STRING_GLOBAL "global"
+#define MAPLE_LOCALE_STRING_KOREA "korea"
+#define MAPLE_LOCALE_STRING_JAPAN "japan"
+#define MAPLE_LOCALE_STRING_CHINA "china"
+#define MAPLE_LOCALE_STRING_EUROPE "europe"
+#define MAPLE_LOCALE_STRING_THAILAND "thailand"
+#define MAPLE_LOCALE_STRING_TAIWAN "taiwan"
+#define MAPLE_LOCALE_STRING_SEA "sea"
+#define MAPLE_LOCALE_STRING_BRAZIL "brazil"
+
+// Use preprocessors ONLY for version-specific code purposes, prefer the strongly typed members of MapleVersion namespace otherwise
+#define MAPLE_LOCALE MAPLE_LOCALE_GLOBAL
+#define MAPLE_LOCALE_STRING MAPLE_LOCALE_STRING_GLOBAL
 #define MAPLE_VERSION 75
-// Use ONLY for version-specific code purposes
-// Incidentally, your locale may have vastly different version-specific code
 
 namespace Locales {
-	const int8_t Korea = 0x01;
-	const int8_t Japan = 0x03;
-	const int8_t China = 0x05;
-	const int8_t Sea = 0x07;
-	const int8_t Thailand = 0x07;
-	const int8_t Global = 0x08;
-	const int8_t Europe = 0x09;
-	const int8_t Brazil = 0x09;
-	const int8_t Taiwan = 0x3C;
-	const int8_t Vietnam = 0x00; // Unk
+	const int8_t Korea = MAPLE_LOCALE_KOREA;
+	const int8_t Japan = MAPLE_LOCALE_JAPAN;
+	const int8_t China = MAPLE_LOCALE_CHINA;
+	const int8_t Sea = MAPLE_LOCALE_SEA;
+	const int8_t Thailand = MAPLE_LOCALE_THAILAND;
+	const int8_t Global = MAPLE_LOCALE_GLOBAL;
+	const int8_t Europe = MAPLE_LOCALE_EUROPE;
+	const int8_t Brazil = MAPLE_LOCALE_BRAZIL;
+	const int8_t Taiwan = MAPLE_LOCALE_TAIWAN;
 
-	const int8_t KoreaTest = 0x02;
-	const int8_t ChinaTest = 0x04;
-	const int8_t GlobalTest = 0x05;
-	const int8_t TaiwanTest = 0x06;
-	const int8_t EuropeTest = 0x00; // Unk
-	const int8_t JapanTest = 0x00; // Unk
+	const int8_t KoreaTest = MAPLE_LOCALE_KOREA_TEST;
+	const int8_t ChinaTest = MAPLE_LOCALE_CHINA_TEST;
+	const int8_t GlobalTest = MAPLE_LOCALE_GLOBAL_TEST;
+	const int8_t TaiwanTest = MAPLE_LOCALE_TAIWAN_TEST;
 }
 
 namespace PatchLocations {
 	const string None = "";
 	const string Global = "0";
-	const string Korea = ""; // Unk
-	const string Japan = ""; // Unk
-	const string China = ""; // Unk
-	const string Sea = ""; // Unk
-	const string Thailand = ""; // Unk
-	const string Europe = ""; // Unk
-	const string Brazil = ""; // Unk
-	const string Taiwan = ""; // Unk
-	const string Vietnam = ""; // Unk
 
 	const string GlobalTest = "0";
-	const string KoreaTest = ""; // Unk
-	const string ChinaTest = ""; // Unk
-	const string TaiwanTest = ""; // Unk
-	const string EuropeTest = ""; // Unk
-	const string JapanTest = ""; // Unk
 }
 
 namespace MapleVersion {
 	const uint16_t Version = MAPLE_VERSION;
-	const int8_t Locale = Locales::Global;
-	const string LocaleString = MAPLE_LOCALE;
+	const int8_t Locale = MAPLE_LOCALE;
+	const string LocaleString = MAPLE_LOCALE_STRING;
 	const bool TestServer = false;
-
 	const string PatchLocation = PatchLocations::Global;
 }
