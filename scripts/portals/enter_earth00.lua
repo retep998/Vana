9e04474fc3f8a412f9b1bf53a169e70645cd97c8
LazyBui <LazyBui@09c1253f-3d09-8e40-9719-4320e56601b5>
2014-02-09 13:06:03 +0000

    - Adding portals between Nautilus and Omega Sector
    git-svn-id: svn://70.113.44.214/VanaMirror/branches/buis_vana_overhaul@3314 09c1253f-3d09-8e40-9719-4320e56601b5

---
+++ scripts/portals/enter_earth00.lua
@@ -0,0 +1,26 @@
+--[[
+Copyright (C) 2008-2014 Vana Development Team
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; version 2
+of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+--]]
+-- Warp from Nautilus to Omega Sector
+
+if getItemAmount(4031890) == 0 then
+	showMessage("You need a Warp Card to use the warp machine.", env_redMessage);
+else
+	giveItem(4031890, -1);
+	playPortalSe();
+	setMap(221000300, "earth00");
+end
