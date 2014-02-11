acb3138d7923ab2815377c3545a54a91e1abe2fe
LazyBui <LazyBui@09c1253f-3d09-8e40-9719-4320e56601b5>
2014-02-11 20:38:06 +0000

    - Increasing accuracy on Dolphin Taxi for some quest items
    git-svn-id: svn://70.113.44.214/VanaMirror/branches/buis_vana_overhaul@3318 09c1253f-3d09-8e40-9719-4320e56601b5

--- scripts/npcs/aqua_taxi.lua
+++ scripts/npcs/aqua_taxi.lua
@@ -17,16 +17,37 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 --]]
 -- Dolphin (Aquarium)
 
-prices = {1000, 10000};
+prices = nil;
+if getJob() == 0 then
+	prices = {100, 1000};
+else
+	prices = {1000, 10000};
+end
 maps = {230030200, 251000100};
 
-addText("Oceans are all connected to each other. Places you can't reach by foot can be easily reached oversea. How about taking #bDolphin Taxi#k with us today? \r\n");
-addText("#L0# Go to #bThe Sharp Unknown#k after paying 1000mesos.#l\r\n");
-addText("#L1# Go to #bHerb Town#k after paying 10000mesos.#l");
+addText("Oceans are all connected to each other. ");
+addText("Places you can't reach by foot can be easily reached oversea. ");
+addText("How about taking #bDolphin Taxi#k with us today? ");
+if getJob() == 0 then
+	addText("We have special tickets with a 90% discount for Beginners! ");
+end
+addText("\r\n");
+if getItemAmount(4031242) > 0 then
+	addText("#L0# Go to #bThe Sharp Unknown#k using #b#t4031242##k.#l\r\n");
+else
+	addText("#L0# Go to #bThe Sharp Unknown#k after paying " .. prices[1] .. "mesos.#l\r\n");
+end
+
+addText("#L1# Go to #bHerb Town#k after paying " .. prices[2] .. "mesos.#l");
 where = askChoice() + 1;
 
-if giveMesos(-prices[where]) then -- Go to Sharp Unknown/Herb Town
-	setMap(maps[where]);
+if (where == 1 and getItemAmount(4031242) > 0 and giveItem(4031242, -1)) or giveMesos(-prices[where]) then -- Go to Sharp Unknown/Herb Town
+	map = maps[where];
+	portal = nil;
+	if map == 230030200 then
+		portal = "st00";
+	end
+	setMap(map, portal);
 else
 	addText("I don't think you have enough money...");
 	sendNext();
