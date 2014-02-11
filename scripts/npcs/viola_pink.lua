9acf22484235379cc4f232f4587208a003236f42
LazyBui <LazyBui@09c1253f-3d09-8e40-9719-4320e56601b5>
2014-02-11 20:38:58 +0000

    - Adding John's jump quests
    git-svn-id: svn://70.113.44.214/VanaMirror/branches/buis_vana_overhaul@3319 09c1253f-3d09-8e40-9719-4320e56601b5

---
+++ scripts/npcs/viola_pink.lua
@@ -0,0 +1,41 @@
+--[[
+Copyright (C) 2008-2014 Vana Development Team
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; version 2
+of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+--]]
+-- A Pile of Pink Flowers
+
+-- TODO FIXME implement distance properly
+
+if isQuestActive(2052) then
+	item = 4031025;
+	if hasOpenSlotsFor(item, 10) then
+		giveItem(item, 10);
+		setMap(105040300);
+	else
+		addText("Sorry, but your etc. inventory is full. Please make some space and try again.");
+		sendNext();
+	end
+else
+	items = {4010000, 4010001, 4010002, 4010003, 4010004, 4010005};
+	item = items[getRandomNumber(#items)];
+	if hasOpenSlotsFor(item, 2) then
+		giveItem(item, 2);
+		setMap(105040300);
+	else
+		addText("Sorry, but your etc. inventory is full. Please make some space and try again.");
+		sendNext();
+	end
+end
