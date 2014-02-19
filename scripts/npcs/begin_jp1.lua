9d88b6257cbce9364b50510c18cdc94b8a864ade
LazyBui <LazyBui@23f3cdc0-e6d1-42be-a0c1-51f4125517bb>
2014-02-19 03:13:44 +0000

    - Updating some more NPCs
    git-svn-id: https://dev.chiasoft.net:8443/svn/svn_vana/branches/buis_vana_overhaul@3331 23f3cdc0-e6d1-42be-a0c1-51f4125517bb

--- scripts/npcs/begin_jp1.lua
+++ scripts/npcs/begin_jp1.lua
@@ -17,13 +17,19 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 --]]
 --
 
+dofile("scripts/lua_functions/npcHelper.lua");
+
 map = getMap();
 if map == 0 then
-	addText("Welcome to the world of MapleStory. The purpose of this training camp is to help beginners. Would you like to enter this training camp? Some people start their journey without taking the training program. But I strongly recommend you take the training program first.");
-	decision = askYesNo();
+	addText("Welcome to the world of MapleStory. ");
+	addText("The purpose of this training camp is to help beginners. ");
+	addText("Would you like to enter this training camp? Some people start their journey without taking the training program. ");
+	addText("But I strongly recommend you take the training program first.");
+	answer = askYesNo();
 
-	if decision == 1 then
-		addText("Ok then, I will let you enter the training camp. Please follow your instructor's lead.");
+	if answer == answer_yes then
+		addText("Ok then, I will let you enter the training camp. ");
+		addText("Please follow your instructor's lead.");
 		sendNext();
 
 		setMap(1);
@@ -31,8 +37,10 @@ if map == 0 then
 		addText("Do you really wanted to start your journey right away?");
 		skip = askYesNo();
 
-		if skip == 1 then
-			addText("It seems like you want to start your journey without taking the training program. Then, I will let you move on the training ground. Be careful~");
+		if skip == answer_yes then
+			addText("It seems like you want to start your journey without taking the training program. ");
+			addText("Then, I will let you move on the training ground. ");
+			addText("Be careful~");
 			sendNext();
 
 			setMap(40000);
@@ -42,9 +50,11 @@ if map == 0 then
 		end
 	end
 elseif map == 1 then
-	addText("This is the image room where your first training program begins. In this room, you will have an advance look into the job of your choice.");
+	addText("This is the image room where your first training program begins. ");
+	addText("In this room, you will have an advance look into the job of your choice.");
 	sendNext();
 
-	addText("Once you train hard enough, you will be entitled to occupy a job. You can become a Bowman in Henesys, a Magician in Ellinia, a Warrior in Perion, and a Thief in Kerning City..");
+	addText("Once you train hard enough, you will be entitled to occupy a job. ");
+	addText("You can become a Bowman in Henesys, a Magician in Ellinia, a Warrior in Perion, and a Thief in Kerning City..");
 	sendBackOk();
 end
