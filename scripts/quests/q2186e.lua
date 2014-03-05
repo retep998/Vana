e8f7a8cbcb8ee81ed400df350b7b9db9b4300de1
LazyBui <LazyBui@23f3cdc0-e6d1-42be-a0c1-51f4125517bb>
2014-03-05 09:17:54 +0000

    - Adding Abel's Glasses quest end script  - Slight style fix on Mysterious Egg etc
    git-svn-id: https://dev.chiasoft.net:8443/svn/svn_vana/branches/buis_vana_overhaul@3353 23f3cdc0-e6d1-42be-a0c1-51f4125517bb

---
+++ scripts/quests/q2186e.lua
@@ -0,0 +1,42 @@
+--[[
+Copyright (C) 2008-2014 Vana Development Team
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; version 2
+of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+--]]
+-- Abel's Glasses
+
+dofile("scripts/lua_functions/npcHelper.lua");
+
+addText("What? ");
+addText("You found my glasses? ");
+addText("I better put it on first, to make sure that it's really mine. ");
+addText("Oh, it really is mine. ");
+addText("Thank you so much!\r\n\r\n");
+addText(questCompleteIcon() .. "\r\n");
+addText(questItemIcon(2030019, 5) .. "\r\n\r\n");
+addText(questExpIcon(1000) .. "\r\n");
+sendNext();
+
+if hasOpenSlotsFor(2030019, 5) then
+	giveItem(4031853, -1);
+	giveItem(2030019, 5);
+	giveExp(1000);
+	endQuest(2186);
+	
+	addText("Yes...time for some fishing!");
+	sendBackNext();
+else
+	-- TODO FIXME text?
+end
