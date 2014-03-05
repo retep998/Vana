aabac63bb65bbbb7ec7654ff9258806d8017215f
LazyBui <LazyBui@09c1253f-3d09-8e40-9719-4320e56601b5>
2014-03-05 09:16:09 +0000

    - Adding various Nautilus quests
    git-svn-id: svn://70.113.44.214/VanaMirror/branches/buis_vana_overhaul@3352 09c1253f-3d09-8e40-9719-4320e56601b5

---
+++ scripts/npcs/nautil_Abel1.lua
@@ -0,0 +1,45 @@
+--[[
+Copyright (C) 2008-2014 Vana Development Team
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; version 2
+of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+--]]
+-- Bush
+
+dofile("scripts/lua_functions/npcHelper.lua");
+
+if isQuestActive(2186) and getItemAmount(4031853) == 0 then
+	if getNpcId() == 1094004 then
+		if hasOpenSlotsFor(4031853) then
+			giveItem(4031853, 1);
+			addText("I found Abel's glasses.");
+			sendNext();
+		else
+			-- TODO FIXME text?
+		end
+	else
+		items = {4031854, 4031855};
+		item = items[getRandomNumber(#items)];
+		if hasOpenSlotsFor(item) then
+			giveItem(item, 1);
+			addText("I found a pair of glasses, but it doesn't seem to be Abel's. ");
+			addText("Abel's pair is horn-rimmed...");
+			sendOk();
+		else
+			-- TODO FIXME text?
+		end
+	end
+else
+	-- TODO FIXME text?
+end
---
+++ scripts/npcs/nautil_letter.lua
@@ -0,0 +1,42 @@
+--[[
+Copyright (C) 2008-2014 Vana Development Team
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; version 2
+of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+--]]
+-- Trash Can near Muirhat
+
+dofile("scripts/lua_functions/npcHelper.lua");
+
+if not isQuestActive(2162) and not isQuestCompleted(2162) then
+	addText("A half-written letter...maybe it's important! ");
+	addText("Should I take a look?");
+	sendNext();
+
+	if getItemAmount(4031839) > 0 then
+		addText("I've already picked one up. ");
+		addText("I don't think I'll need to pick up another one.");
+		sendOk();
+	else
+		if not hasOpenSlotsFor(4031839, 1) then
+			-- TODO FIXME text?
+		else
+			giveItem(4031839, 1);
+			addText("I can barely maket his out...but it reads Kyrin.");
+			sendOk();
+		end
+	end
+else
+	-- TODO FIXME text?
+end
