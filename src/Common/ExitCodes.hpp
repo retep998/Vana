211e29ab0be2ee77c7b79ccc898e39cff076c96c
LazyBui <LazyBui@09c1253f-3d09-8e40-9719-4320e56601b5>
2014-06-16 16:21:51 +0000

    - Improving exit code commenting to take care of the usual formats that somebody might use to search for exits
    git-svn-id: svn://24.28.66.222/VanaMirror/branches/buis_vana_overhaul@3429 09c1253f-3d09-8e40-9719-4320e56601b5

--- src/Common/ExitCodes.hpp
+++ src/Common/ExitCodes.hpp
@@ -39,9 +39,21 @@ namespace ExitCodes {
 		ForcedByGm = 14,
 	};
 	// Comments for easy searching
-	// exit(0) exit(1) exit(2) exit(3) exit(4)
-	// exit(5) exit(6) exit(7) exit(8) exit(9)
-	// exit(10) exit(11) exit(12) exit(13) exit(14)
+	// exit(0) exit(0x0) exit(0x00) exit(0x00000000)
+	// exit(1) exit(0x1) exit(0x01) exit(0x00000001)
+	// exit(2) exit(0x2) exit(0x02) exit(0x00000002)
+	// exit(3) exit(0x3) exit(0x03) exit(0x00000003)
+	// exit(4) exit(0x4) exit(0x04) exit(0x00000004)
+	// exit(5) exit(0x5) exit(0x05) exit(0x00000005)
+	// exit(6) exit(0x6) exit(0x06) exit(0x00000006)
+	// exit(7) exit(0x7) exit(0x07) exit(0x00000007)
+	// exit(8) exit(0x8) exit(0x08) exit(0x00000008)
+	// exit(9) exit(0x9) exit(0x09) exit(0x00000009)
+	// exit(10) exit(0xA) exit(0x0A) exit(0x0000000A)
+	// exit(11) exit(0xB) exit(0x0B) exit(0x0000000B)
+	// exit(12) exit(0xC) exit(0x0C) exit(0x0000000C)
+	// exit(13) exit(0xD) exit(0x0D) exit(0x0000000D)
+	// exit(14) exit(0xE) exit(0x0E) exit(0x0000000E)
 
 	auto exit(exit_code_t code) -> void;
 }
