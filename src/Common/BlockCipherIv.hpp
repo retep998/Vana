e39ea6d91372c998fd6b976fbaeffc3e83944f35
LazyBui <LazyBui@23f3cdc0-e6d1-42be-a0c1-51f4125517bb>
2016-05-01 03:30:58 +0000

    - Copyright updates
    git-svn-id: https://dev.chiasoft.net:8443/svn/svn_vana/branches/buis_vana_overhaul@3532 23f3cdc0-e6d1-42be-a0c1-51f4125517bb

--- src/Common/BlockCipherIv.hpp
+++ src/Common/BlockCipherIv.hpp
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2008-2015 Vana Development Team
+Copyright (C) 2008-2016 Vana Development Team
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
