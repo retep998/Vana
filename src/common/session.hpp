5024737ec8e559f408453475180829f77416c61a
LazyBui <Lazy.Bui@Gmail.com>
2016-10-16 17:04:05 -0500

    - Removing decoder as it's no longer actually used

--- src/common/session.hpp
+++ src/common/session.hpp
@@ -17,7 +17,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 #pragma once
 
-#include "common/decoder.hpp"
 #include "common/config/ping.hpp"
 #include "common/connection_type.hpp"
 #include "common/ip.hpp"
